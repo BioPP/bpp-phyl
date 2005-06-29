60af31dd63547113d32ed7e69c1c7343f7ce8b03
jdutheil <jdutheil@a3c5091b-e48a-49e8-af28-ccc209bd1d1c>
2005-06-29 15:04:14 +0000

    Forgot to add this file...

---
+++ Phyl/DistanceMatrix.h
@@ -0,0 +1,149 @@
+//
+// File: DistanceMatrix.h
+// Created on: Wed jun 08 10:39 2005
+//
+
+/*
+Copyright ou © ou Copr. CNRS, (16 Novembre 2004) 
+
+Julien.Dutheil@univ-montp2.fr
+
+Ce logiciel est un programme informatique servant à fournir des classes
+pour l'analyse de données phylogénétiques.
+
+Ce logiciel est régi par la licence CeCILL soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL telle que diffusée par le CEA, le CNRS et l'INRIA 
+sur le site "http://www.cecill.info".
+
+En contrepartie de l'accessibilité au code source et des droits de copie,
+de modification et de redistribution accordés par cette licence, il n'est
+offert aux utilisateurs qu'une garantie limitée.  Pour les mêmes raisons,
+seule une responsabilité restreinte pèse sur l'auteur du programme,  le
+titulaire des droits patrimoniaux et les concédants successifs.
+
+A cet égard  l'attention de l'utilisateur est attirée sur les risques
+associés au chargement,  à l'utilisation,  à la modification et/ou au
+développement et à la reproduction du logiciel par l'utilisateur étant 
+donné sa spécificité de logiciel libre, qui peut le rendre complexe à 
+manipuler et qui le réserve donc à des développeurs et des professionnels
+avertis possédant  des  connaissances  informatiques approfondies.  Les
+utilisateurs sont donc invités à charger  et  tester  l'adéquation  du
+logiciel à leurs besoins dans des conditions permettant d'assurer la
+sécurité de leurs systèmes et ou de leurs données et, plus généralement, 
+à l'utiliser et l'exploiter dans les mêmes conditions de sécurité. 
+
+Le fait que vous puissiez accéder à cet en-tête signifie que vous avez 
+pris connaissance de la licence CeCILL, et que vous en avez accepté les
+termes.
+*/
+
+/*
+Copyright or © or Copr. CNRS, (November 16, 2004)
+
+Julien.Dutheil@univ-montp2.fr
+
+This software is a computer program whose purpose is to provide classes
+for phylogenetic data analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _DISTANCEMATRIX_H_
+#define _DISTANCEMATRIX_H_
+
+// From the STL:
+#include <vector>
+#include <string>
+
+// From Utils
+#include <Utils/Exceptions.h>
+
+// From NumCalc
+#include <NumCalc/Matrix.h>
+
+class DistanceMatrix: public virtual RowMatrix<double> {
+
+	private:
+		vector<string> _names;
+
+	public:
+		DistanceMatrix(const vector<string> & names): RowMatrix<double>(names.size(), names.size()), _names(names)
+		{
+			reset();
+		}
+
+		DistanceMatrix(unsigned int n): RowMatrix<double>(n, n)
+		{
+			_names.resize(n);
+			for(unsigned int i = 0; i < n; i++) _names[i] = "Taxon " + i;
+		}
+
+		~DistanceMatrix() {}
+
+		DistanceMatrix(const DistanceMatrix & dist): RowMatrix<double>(dist), _names(dist._names)	{}
+
+		DistanceMatrix & operator=(const DistanceMatrix & dist)
+		{
+			unsigned int n = dist.size();
+			resize(n, n);
+			for(unsigned int i = 0; i < n; i++) {
+				for(unsigned int j = 0; j < n; j++) {
+					operator()(i, j) = dist(i, j);
+				}
+			}
+			_names = dist._names;
+			return *this;
+		}
+		
+	public:
+
+		void reset()
+		{
+			unsigned int n = size();
+			for(unsigned int i = 0; i < n; i++) {
+				for(unsigned int j = 0; j < n; j++) {
+					operator()(i, j) = 0;
+				}
+			}
+		}
+		
+		unsigned int size() const { return _names.size(); }
+		vector<string> getNames() const { return _names; }
+		string getName(unsigned int i) const { return _names[i]; }
+		void setName(unsigned int i, const string & name) throw (IndexOutOfBoundsException)
+		{
+			if(i >= size()) throw IndexOutOfBoundsException("DistanceMatrix::setName()", i, 0, size());
+			_names[i] = name;
+		}
+
+};
+
+#endif //_DISTANCEMATRIX_H_
+
+
