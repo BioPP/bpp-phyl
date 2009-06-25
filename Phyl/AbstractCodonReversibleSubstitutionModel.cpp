6a6f0ceae6356e7394241ed427e8fe01ae41647d
gueguen <gueguen@a3c5091b-e48a-49e8-af28-ccc209bd1d1c>
2009-06-25 04:38:42 +0000

    Abstract basic codon models

---
+++ Phyl/AbstractCodonReversibleSubstitutionModel.cpp
@@ -0,0 +1,132 @@
+//
+// File: CodonReversibleSubstitutionModel.cpp
+// Created by:  Laurent Gueguen
+// Created on: Feb 2009
+//
+
+/*
+Copyright or © or Copr. CNRS, (November 16, 2004)
+This software is a computer program whose purpose is to provide classes
+for phylogenetic data analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "AbstractCodonReversibleSubstitutionModel.h"
+
+using namespace bpp;
+
+using namespace std;
+
+/******************************************************************************/
+
+AbstractCodonReversibleSubstitutionModel::AbstractCodonReversibleSubstitutionModel(const CodonAlphabet* palph,
+                                                                                   NucleotideSubstitutionModel* pmod,
+                                                                                   const std::string& st)  : AbstractWordReversibleSubstitutionModel(palph,st)
+{
+  enableEigenDecomposition(1);
+
+  int i;
+  for (i=0;i< 3; i++){
+    _VAbsRevMod.push_back(pmod);
+    _VnestedPrefix.push_back(pmod->getNamespace());
+  }
+  
+  pmod->setNamespace(st+"012_"+_VnestedPrefix[0]);
+  addParameters_(pmod->getParameters());
+  
+  _rate=new double[3];
+  for (i=0;i< 3; i++)
+    _rate[i]=1.0/3; 
+}
+
+AbstractCodonReversibleSubstitutionModel::AbstractCodonReversibleSubstitutionModel(const CodonAlphabet* palph,
+                                                                                   NucleotideSubstitutionModel* pmod1,  
+                                                                                   NucleotideSubstitutionModel* pmod2,
+                                                                                   NucleotideSubstitutionModel* pmod3,
+                                                                                   const std::string& st) : AbstractWordReversibleSubstitutionModel(palph,st)
+{
+  enableEigenDecomposition(1);
+
+  if ((pmod1==pmod2) || (pmod2==pmod3) || (pmod1==pmod3)){
+    int i;
+    for (i=0;i< 3; i++){
+      _VAbsRevMod.push_back(pmod1);
+      _VnestedPrefix.push_back(pmod1->getNamespace());
+    }
+  
+    pmod1->setNamespace(st+"012_"+_VnestedPrefix[0]);
+    addParameters_(pmod1->getParameters());
+  }
+  else {
+    _VAbsRevMod.push_back(pmod1);
+    _VnestedPrefix.push_back(pmod1->getNamespace());
+    _VAbsRevMod[0]->setNamespace(st+"0_"+_VnestedPrefix[0]);
+    addParameters_(pmod1->getParameters());
+    
+    _VAbsRevMod.push_back(pmod2);
+    _VnestedPrefix.push_back(pmod2->getNamespace());
+    _VAbsRevMod[1]->setNamespace(st+"1_"+_VnestedPrefix[1]);
+    addParameters_(pmod2->getParameters());
+    
+    _VAbsRevMod.push_back(pmod3);
+    _VnestedPrefix.push_back(pmod3->getNamespace());
+    _VAbsRevMod[2]->setNamespace(st+"2_"+_VnestedPrefix[2]);
+    addParameters_(pmod3->getParameters());
+  }
+  
+  _rate=new double[3];
+  for (unsigned int i=0;i< 3; i++)
+    _rate[i]=1.0/3;
+  
+}
+  
+AbstractCodonReversibleSubstitutionModel::AbstractCodonReversibleSubstitutionModel(const AbstractCodonReversibleSubstitutionModel& wrsm) :
+  AbstractWordReversibleSubstitutionModel(wrsm.alphabet_,wrsm.getNamespace())
+{
+  enableEigenDecomposition(1);
+
+  _VAbsRevMod.push_back(wrsm._VAbsRevMod[0]);
+  _VnestedPrefix.push_back(wrsm._VnestedPrefix[0]);
+
+  _VAbsRevMod.push_back(wrsm._VAbsRevMod[1]);
+  _VnestedPrefix.push_back(wrsm._VnestedPrefix[1]);
+
+  _VAbsRevMod.push_back(wrsm._VAbsRevMod[2]);
+  _VnestedPrefix.push_back(wrsm._VnestedPrefix[2]);
+
+  addParameters_(wrsm.getParameters());
+
+  _rate=new double[3];
+  for (unsigned int i=0;i< 3; i++)
+    _rate[i]=1.0/3;
+}
+
+AbstractCodonReversibleSubstitutionModel::~AbstractCodonReversibleSubstitutionModel()
+{
+}
+
---
+++ Phyl/AbstractCodonReversibleSubstitutionModel.h
@@ -0,0 +1,109 @@
+//
+// File: AbstractCodonReversibleSubstitutionModel.h
+// Created by: Laurent Gueguen
+// Created on: Tue Dec 24 11:03:53 2003
+//
+
+/*
+Copyright or © or Copr. CNRS, (November 16, 2004)
+
+This software is a computer program whose purpose is to provide classes
+for phylogenetic data analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _ABSTRACTCODONREVERSIBLESUBSTITUTIONMODEL_H_
+#define _ABSTRACTCODONREVERSIBLESUBSTITUTIONMODEL_H_
+
+#include "AbstractWordReversibleSubstitutionModel.h"
+#include "NucleotideSubstitutionModel.h"
+
+// From SeqLib:
+#include <Seq/CodonAlphabet.h>
+
+namespace bpp
+{
+
+/**
+ * @brief Abstract class for reversible substitution models on codons.
+ *
+ * Objects of this class are built from three reversible substitution
+ * models of NucleicAlphabets. No model is directly accessible. </p>
+ *
+ * Only substitutions with one letter changed are accepted. </p>
+ *
+ * There is one substitution per word per unit of time
+ * on the equilibrium frequency, and each position has its specific rate.
+ *
+ */
+  
+class AbstractCodonReversibleSubstitutionModel :
+  public AbstractWordReversibleSubstitutionModel
+{
+public:
+
+  /**
+   *@brief Build a new AbstractCodonReversibleSubstitutionModel object from
+   *a pointer to an AbstractReversibleSubstitutionModel. 
+   *
+   *@param pointer to a CodonAlphabet
+   *@param pmodel is a pointer to the
+   *AbstractReversibleSubstitutionModel to use in the three positions.
+   */
+  
+  AbstractCodonReversibleSubstitutionModel(const CodonAlphabet*,
+                                            NucleotideSubstitutionModel*,
+                                           const std::string&);
+  
+  /**
+   *@brief Build a new AbstractReversibleSubstitutionModel object
+   *from three pointers to AbstractReversibleSubstitutionModels. 
+   *
+   *@param pointer to a CodonAlphabet
+   *@param pmodel1, pmodel2 and pmodel3 are pointers to the
+   *AbstractReversibleSubstitutionModel to use in the three positions.
+   * All the models must be different objects to avoid redondant
+   * parameters.
+   */
+
+  AbstractCodonReversibleSubstitutionModel(const CodonAlphabet*,
+                                            NucleotideSubstitutionModel*,
+                                            NucleotideSubstitutionModel*, 
+                                            NucleotideSubstitutionModel*,
+                                           const std::string&);
+
+
+  AbstractCodonReversibleSubstitutionModel(const AbstractCodonReversibleSubstitutionModel&);
+
+  virtual ~AbstractCodonReversibleSubstitutionModel();  
+};
+
+} //end of namespace bpp.
+
+#endif	
+
