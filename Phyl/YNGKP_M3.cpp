b740d6cae993a3e303d11841f933bce3324211bd
gueguen <gueguen@a3c5091b-e48a-49e8-af28-ccc209bd1d1c>
2010-05-27 20:27:44 +0000

    addition the M3 model of codeml

---
+++ Phyl/YNGKP_M3.cpp
@@ -0,0 +1,165 @@
+//
+// File: YNGKP_M3.cpp
+// Created by:  Laurent Gueguen
+// Created on: May 2010
+//
+
+/*
+Copyright or © or Copr. CNRS, (November 16, 2004)
+This software is a computer program whose purpose is to provide classes
+for phylogenetic data analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "YNGKP_M3.h"
+#include "YN98.h"
+#include "FrequenciesSet.h"
+
+#include <NumCalc/SimpleDiscreteDistribution.h>
+#include <NumCalc/NumConstants.h>
+
+using namespace bpp;
+
+using namespace std;
+
+/******************************************************************************/
+
+YNGKP_M3::YNGKP_M3(const GeneticCode* gc, FrequenciesSet* codonFreqs, unsigned int nbOmega) :
+  MixedSubstitutionModel(gc->getSourceAlphabet(), "YNGKP_M3."), pmixmodel_(0),
+  mapParNamesFromPmodel_(), lParPmodel_()
+{
+  if (nbOmega<1)
+    throw Exception("At least one omega is necessary in the YNGKP_M3 model");
+
+  addParameter_(Parameter("YNGKP_M3.omega0", 0.5, new IncludingExcludingInterval(0.0001,999), true));
+  for (unsigned int i = 1; i < nbOmega;i++)
+    addParameter_(Parameter("YNGKP_M3.delta"+TextTools::toString(i), 0.5, &Parameter::R_PLUS_STAR));
+
+  for (unsigned int i = 1; i < nbOmega;i++)
+    addParameter_(Parameter("YNGKP_M3.theta"+TextTools::toString(i), 1./(nbOmega-i+1), &Parameter::PROP_CONSTRAINT_IN));
+  
+  addParameter_(Parameter("YNGKP_M3.kappa", 1, &Parameter::R_PLUS_STAR));
+
+  ParameterList pl=codonFreqs->getParameters();
+
+  for (unsigned int i=0;i<pl.size();i++)
+    if (pl[i].getConstraint())
+      addParameter_(Parameter("YNGKP_M3.freq_"+pl[i].getName(), pl[i].getValue(), pl[i].getConstraint()->clone(), true));
+    else
+      addParameter_(Parameter("YNGKP_M3.freq_"+pl[i].getName(), pl[i].getValue()));      
+
+  /*************************/
+
+  vector<double> v1, v2;
+  v1.push_back(getParameterValue("YNGKP_M3.omega0"));
+  for (unsigned int i=1;i<nbOmega;i++)
+    v1.push_back(getParameterValue("YNGKP_M3.delta"+TextTools::toString(i))+v1[i-1]);
+
+  double x=1;
+  for (unsigned int i=1;i<nbOmega;i++){
+    v2.push_back(getParameterValue("YNGKP_M3.theta"+TextTools::toString(i))*x);
+    x*=1-getParameterValue("YNGKP_M3.theta"+TextTools::toString(i));
+  }
+  v2.push_back(x);
+
+  SimpleDiscreteDistribution* psdd=new SimpleDiscreteDistribution(v1,v2);
+
+  map<string, DiscreteDistribution*> mpdd;
+  mpdd["omega"]=psdd;
+
+  pmixmodel_= new MixtureOfSubstitutionModels(gc->getSourceAlphabet(),
+                                              new YN98(gc, codonFreqs),
+                                              mpdd);
+  delete psdd;
+
+  // mapping the parameters
+  
+  pl=pmixmodel_->getParameters();
+  for (unsigned int i=0;i<pl.size();i++)
+    lParPmodel_.addParameter(Parameter(pl[i]));
+
+  vector<std::string> v=dynamic_cast<YN98*>(pmixmodel_->getNModel(0))->getFreq().getParameters().getParameterNames();
+  for (unsigned int i=0;i<v.size();i++)
+    mapParNamesFromPmodel_[v[i]]=getParameterNameWithoutNamespace("YNGKP_M3."+v[i].substr(5));
+
+  mapParNamesFromPmodel_["YN98.kappa"]="YNGKP_M3.kappa";
+  
+  for (unsigned int i=1;i<nbOmega;i++)
+    mapParNamesFromPmodel_["YN98.omega_Simple.theta"+TextTools::toString(i)]="YNGKP_M3.theta"+TextTools::toString(i);
+
+
+  mapParNamesFromPmodel_["YN98.omega_Simple.V1"]="YNGKP_M3.omega0";
+  for (unsigned int i=1;i<nbOmega;i++)
+    mapParNamesFromPmodel_["YN98.omega_Simple.V"+TextTools::toString(i+1)]="YNGKP_M3.delta"+TextTools::toString(i);
+
+  updateMatrices();
+}
+
+YNGKP_M3::YNGKP_M3(const YNGKP_M3& mod2) : MixedSubstitutionModel(mod2),
+                                           pmixmodel_(new MixtureOfSubstitutionModels(*mod2.pmixmodel_)),
+                                           mapParNamesFromPmodel_(mod2.mapParNamesFromPmodel_),
+                                           lParPmodel_(mod2.lParPmodel_)
+{
+  
+}
+
+YNGKP_M3& YNGKP_M3::operator=(const YNGKP_M3& mod2)
+{
+  MixedSubstitutionModel::operator=(mod2);
+
+  pmixmodel_=new MixtureOfSubstitutionModels(*mod2.pmixmodel_);
+  mapParNamesFromPmodel_=mod2.mapParNamesFromPmodel_;
+  lParPmodel_=mod2.lParPmodel_;
+  
+  return *this;
+}
+
+YNGKP_M3::~YNGKP_M3()
+{
+  if (pmixmodel_)
+    delete pmixmodel_;
+}
+
+void YNGKP_M3::updateMatrices()
+{
+  for (unsigned int i=0;i<lParPmodel_.size();i++)
+    if (hasParameter(mapParNamesFromPmodel_[lParPmodel_[i].getName()])){
+      if (lParPmodel_[i].getName()[18]=='V'){
+        unsigned int ind=TextTools::toInt(lParPmodel_[i].getName().substr(19));
+        double x=getParameterValue("YNGKP_M3.omega0");
+        for (unsigned j=1;j<ind;j++)
+          x+=getParameterValue("YNGKP_M3.delta"+TextTools::toString(j));
+        lParPmodel_[i].setValue(x);
+      }
+      else{
+        lParPmodel_[i].setValue(getParameterValue(mapParNamesFromPmodel_[lParPmodel_[i].getName()]));
+      }
+    }
+  
+  pmixmodel_->matchParametersValues(lParPmodel_);
+}
