fa514a7a64fc4d8a0465027daef05b4773ecdf06
Laurent Gueguen <Laurent.Gueguen@univ-lyon1.fr>
2011-09-14 13:58:31 +0200

    addition of binary model

--- src/Bpp/Phyl/App/PhylogeneticsApplicationTools.cpp
+++ src/Bpp/Phyl/App/PhylogeneticsApplicationTools.cpp
@@ -806,6 +806,13 @@ SubstitutionModel* PhylogeneticsApplicationTools::getSubstitutionModelDefaultIns
           throw Exception("'name' argument missing for user-defined substitution model.");
         model = new UserProteinSubstitutionModel(alpha, args["file"], prefix);
       }
+      else if (AlphabetTools::isBinaryAlphabet(alphabet))
+        {
+          const BinaryAlphabet* alpha = dynamic_cast<const BinaryAlphabet*>(alphabet);
+
+          if (modelName == "Binary")
+            model = new BinarySubstitutionModel(alpha);
+        }
       else
         throw Exception("Model '" + modelName + "' unknown.");
     }
