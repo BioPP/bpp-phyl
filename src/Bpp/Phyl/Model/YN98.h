5c38c8cbce92cc3add3a181f2a054bee2e0bb3a1
Julien Y. Dutheil <jy.dutheil@gmail.com>
2011-09-28 13:16:33 +0200

    Source formating.

--- src/Bpp/Phyl/Model/YN98.h
+++ src/Bpp/Phyl/Model/YN98.h
@@ -5,37 +5,37 @@
 //
 
 /*
-Copyright or © or Copr. CNRS, (November 16, 2004)
+   Copyright or © or Copr. Bio++ Development Team, (November 16, 2004)
 
-This software is a computer program whose purpose is to provide classes
-for phylogenetic data analysis.
+   This software is a computer program whose purpose is to provide classes
+   for phylogenetic data analysis.
 
-This software is governed by the CeCILL  license under French law and
-abiding by the rules of distribution of free software.  You can  use, 
-modify and/ or redistribute the software under the terms of the CeCILL
-license as circulated by CEA, CNRS and INRIA at the following URL
-"http://www.cecill.info". 
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use,
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info".
 
-As a counterpart to the access to the source code and  rights to copy,
-modify and redistribute granted by the license, users are provided only
-with a limited warranty  and the software's author,  the holder of the
-economic rights,  and the successive licensors  have only  limited
-liability. 
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability.
 
-In this respect, the user's attention is drawn to the risks associated
-with loading,  using,  modifying and/or developing or reproducing the
-software by the user in light of its specific status of free software,
-that may mean  that it is complicated to manipulate,  and  that  also
-therefore means  that it is reserved for developers  and  experienced
-professionals having in-depth computer knowledge. Users are therefore
-encouraged to load and test the software's suitability as regards their
-requirements in conditions enabling the security of their systems and/or 
-data to be ensured and,  more generally, to use and operate it in the 
-same conditions as regards security. 
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or
+   data to be ensured and,  more generally, to use and operate it in the
+   same conditions as regards security.
 
-The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
-*/
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+ */
 
 #ifndef _YN98_H_
 #define _YN98_H_
@@ -45,7 +45,6 @@ knowledge of the CeCILL license and that you accept its terms.
 
 namespace bpp
 {
-
 /**
  * @brief The Yang and Nielsen (1998) substitution model for codons.
  * @author Laurent Guéguen
@@ -55,7 +54,7 @@ namespace bpp
  * between codons. A multiplicative factor accounts for the selective
  * restraints at the amino acid level, depending on the synonymy of
  * the amino acids.
- * 
+ *
  * For codons @f$i=i_1i_2i_3@f$ and @f$j=j_1j_2j_3@f$, the generator
  * term @f$Q_{ij} (i \neq j)@f$ is:
  *
@@ -86,7 +85,7 @@ namespace bpp
  * Reference:
  * -  Yang Z. and Nielsen R. (1998), _Journal of Molecular Evolution_ 46:409--418.
  */
-class YN98:  public AbstractBiblioSubstitutionModel
+class YN98 : public AbstractBiblioSubstitutionModel
 {
 private:
   CodonDistanceFrequenciesSubstitutionModel* pmodel_;
@@ -95,31 +94,24 @@ public:
   YN98(const GeneticCode* gc, FrequenciesSet* codonFreqs);
 
   YN98(const YN98& yn98);
-  
+
   YN98& operator=(const YN98&);
-  
+
   ~YN98();
 
-#ifndef NOVIRTUAL_COV_
-  YN98*
-#else
-  Clonable*
-#endif
-  clone() const { return new YN98(*this); }
+  YN98* clone() const { return new YN98(*this); }
 
 public:
-
   std::string getName() const { return "YN98"; }
-	
-  const AbstractSubstitutionModel* getModel() const { return pmodel_;}
 
-  AbstractSubstitutionModel* getModel() { return pmodel_;}
+  const AbstractSubstitutionModel* getModel() const { return pmodel_; }
+
+  AbstractSubstitutionModel* getModel() { return pmodel_; }
 
   const FrequenciesSet& getFreq() const { return pmodel_->getFreq(); }
-
 };
 
-} //end of namespace bpp.
+} // end of namespace bpp.
 
-#endif	//_YN98_H_
+#endif  // _YN98_H_
 
