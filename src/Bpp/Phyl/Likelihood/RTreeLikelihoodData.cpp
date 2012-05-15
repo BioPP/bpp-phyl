22690b0d9801b544612bbb396119f9a9e38f4917
Julien Dutheil <jy.dutheil@gmail.com>
2012-05-15 12:26:27 +0200

    Started import to new Likelihood directory. - Added TreeLikelihoodData files - Added generic TreeLikelihood interface.

---
+++ src/Bpp/Phyl/Likelihood/RTreeLikelihoodData.cpp
@@ -0,0 +1,286 @@
+//
+// File: RTreeLikelihoodData.cpp
+// Created by: Julien Dutheil
+// Created on: Sat Dec 30 14:20 2006
+// From file HomogeneousTreeLikelihood.cpp
+//
+
+/*
+   Copyright or © or Copr. Bio++ Development Team, (November 16, 2004)
+
+   This software is a computer program whose purpose is to provide classes
+   for phylogenetic data analysis.
+
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use,
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info".
+
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability.
+
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or
+   data to be ensured and,  more generally, to use and operate it in the
+   same conditions as regards security.
+
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+ */
+
+#include "RTreeLikelihoodData.h"
+#include "../PatternTools.h"
+
+// From SeqLib:
+#include <Bpp/Seq/SiteTools.h>
+#include <Bpp/Seq/Container/AlignedSequenceContainer.h>
+#include <Bpp/Seq/Container/SequenceContainerTools.h>
+#include <Bpp/Seq/Container/VectorSiteContainer.h>
+
+using namespace bpp;
+
+/******************************************************************************/
+
+void RTreeLikelihoodData::initLikelihoods(const SiteContainer& sites, const SubstitutionModel& model)
+throw (Exception)
+{
+  if (sites.getNumberOfSequences() == 1) throw Exception("Error, only 1 sequence!");
+  if (sites.getNumberOfSequences() == 0) throw Exception("Error, no sequence!");
+  if (sites.getAlphabet()->getAlphabetType()
+      != model.getAlphabet()->getAlphabetType())
+    throw AlphabetMismatchException("DRTreeLikelihoodData::initLikelihoods. Data and model must have the same alphabet type.",
+                                    sites.getAlphabet(),
+                                    model.getAlphabet());
+  alphabet_ = sites.getAlphabet();
+  nbStates_ = model.getNumberOfStates();
+  nbSites_  = sites.getNumberOfSites();
+  if (shrunkData_) delete shrunkData_;
+  SitePatterns* patterns;
+  if (usePatterns_)
+  {
+    patterns          = initLikelihoodsWithPatterns(tree_->getRootNode(), sites, model);
+    shrunkData_       = patterns->getSites();
+    rootWeights_      = patterns->getWeights();
+    rootPatternLinks_ = patterns->getIndices();
+    nbDistinctSites_  = shrunkData_->getNumberOfSites();
+  }
+  else
+  {
+    patterns          = new SitePatterns(&sites);
+    shrunkData_       = patterns->getSites();
+    rootWeights_      = patterns->getWeights();
+    rootPatternLinks_ = patterns->getIndices();
+    nbDistinctSites_  = shrunkData_->getNumberOfSites();
+    initLikelihoods(tree_->getRootNode(), *shrunkData_, model);
+  }
+  delete patterns;
+}
+
+/******************************************************************************/
+void RTreeLikelihoodData::initLikelihoods(const Node* node, const SiteContainer& sequences, const SubstitutionModel& model) throw (Exception)
+{
+  // Initialize likelihood vector:
+  RTreeLikelihoodNodeData* nodeData = &nodeData_[node->getId()];
+  nodeData->setNode(node);
+  VVVdouble* _likelihoods_node = &nodeData->getLikelihoodArray();
+  VVVdouble* _dLikelihoods_node = &nodeData->getDLikelihoodArray();
+  VVVdouble* _d2Likelihoods_node = &nodeData->getD2LikelihoodArray();
+
+  _likelihoods_node->resize(nbDistinctSites_);
+  _dLikelihoods_node->resize(nbDistinctSites_);
+  _d2Likelihoods_node->resize(nbDistinctSites_);
+
+  for (unsigned int i = 0; i < nbDistinctSites_; i++)
+  {
+    VVdouble* _likelihoods_node_i = &(*_likelihoods_node)[i];
+    VVdouble* _dLikelihoods_node_i = &(*_dLikelihoods_node)[i];
+    VVdouble* _d2Likelihoods_node_i = &(*_d2Likelihoods_node)[i];
+    _likelihoods_node_i->resize(nbClasses_);
+    _dLikelihoods_node_i->resize(nbClasses_);
+    _d2Likelihoods_node_i->resize(nbClasses_);
+    for (unsigned int c = 0; c < nbClasses_; c++)
+    {
+      Vdouble* _likelihoods_node_i_c = &(*_likelihoods_node_i)[c];
+      Vdouble* _dLikelihoods_node_i_c = &(*_dLikelihoods_node_i)[c];
+      Vdouble* _d2Likelihoods_node_i_c = &(*_d2Likelihoods_node_i)[c];
+      _likelihoods_node_i_c->resize(nbStates_);
+      _dLikelihoods_node_i_c->resize(nbStates_);
+      _d2Likelihoods_node_i_c->resize(nbStates_);
+      for (unsigned int s = 0; s < nbStates_; s++)
+      {
+        (*_likelihoods_node_i_c)[s] = 1; // All likelihoods are initialized to 1.
+        (*_dLikelihoods_node_i_c)[s] = 0; // All dLikelihoods are initialized to 0.
+        (*_d2Likelihoods_node_i_c)[s] = 0; // All d2Likelihoods are initialized to 0.
+      }
+    }
+  }
+
+  // Now initialize likelihood values and pointers:
+
+  if (node->isLeaf())
+  {
+    const Sequence* seq;
+    try
+    {
+      seq = &sequences.getSequence(node->getName());
+    }
+    catch (SequenceNotFoundException snfe)
+    {
+      throw SequenceNotFoundException("RTreeLikelihoodData::initTreelikelihoods. Leaf name in tree not found in site conainer: ", (node->getName()));
+    }
+    for (unsigned int i = 0; i < nbDistinctSites_; i++)
+    {
+      VVdouble* _likelihoods_node_i = &(*_likelihoods_node)[i];
+      int state = seq->getValue(i);
+      for (unsigned int c = 0; c < nbClasses_; c++)
+      {
+        Vdouble* _likelihoods_node_i_c = &(*_likelihoods_node_i)[c];
+        double test = 0.;
+        for (unsigned int s = 0; s < nbStates_; s++)
+        {
+          // Leaves likelihood are set to 1 if the char correspond to the site in the sequence,
+          // otherwise value set to 0:
+          // cout << "i=" << i << "\tc=" << c << "\ts=" << s << endl;
+          (*_likelihoods_node_i_c)[s] = model.getInitValue(s, state);
+          test += (*_likelihoods_node_i_c)[s];
+        }
+        if (test < 0.000001) std::cerr << "WARNING!!! Likelihood will be 0 for this site." << std::endl;
+      }
+    }
+  }
+  else
+  {
+    // 'node' is an internal node.
+    std::map<int, std::vector<unsigned int> >* patternLinks__node = &patternLinks_[node->getId()];
+    unsigned int nbSonNodes = node->getNumberOfSons();
+    for (unsigned int l = 0; l < nbSonNodes; l++)
+    {
+      // For each son node,
+      const Node* son = (*node)[l];
+      initLikelihoods(son, sequences, model);
+      std::vector<unsigned int>* patternLinks__node_son = &(*patternLinks__node)[son->getId()];
+
+      // Init map:
+      patternLinks__node_son->resize(nbDistinctSites_);
+
+      for (unsigned int i = 0; i < nbDistinctSites_; i++)
+      {
+        (*patternLinks__node_son)[i] = i;
+      }
+    }
+  }
+}
+
+/******************************************************************************/
+SitePatterns* RTreeLikelihoodData::initLikelihoodsWithPatterns(const Node* node, const SiteContainer& sequences, const SubstitutionModel& model) throw (Exception)
+{
+  SiteContainer* tmp = PatternTools::getSequenceSubset(sequences, *node);
+  SitePatterns* patterns = new SitePatterns(tmp, true);
+  SiteContainer* subSequences = patterns->getSites();
+
+  unsigned int nbSites = subSequences->getNumberOfSites();
+
+  // Initialize likelihood vector:
+  RTreeLikelihoodNodeData* nodeData = &nodeData_[node->getId()];
+  nodeData->setNode(node);
+  VVVdouble* _likelihoods_node = &nodeData->getLikelihoodArray();
+  VVVdouble* _dLikelihoods_node = &nodeData->getDLikelihoodArray();
+  VVVdouble* _d2Likelihoods_node = &nodeData->getD2LikelihoodArray();
+  _likelihoods_node->resize(nbSites);
+  _dLikelihoods_node->resize(nbSites);
+  _d2Likelihoods_node->resize(nbSites);
+
+  for (unsigned int i = 0; i < nbSites; i++)
+  {
+    VVdouble* _likelihoods_node_i = &(*_likelihoods_node)[i];
+    VVdouble* _dLikelihoods_node_i = &(*_dLikelihoods_node)[i];
+    VVdouble* _d2Likelihoods_node_i = &(*_d2Likelihoods_node)[i];
+    _likelihoods_node_i->resize(nbClasses_);
+    _dLikelihoods_node_i->resize(nbClasses_);
+    _d2Likelihoods_node_i->resize(nbClasses_);
+    for (unsigned int c = 0; c < nbClasses_; c++)
+    {
+      Vdouble* _likelihoods_node_i_c = &(*_likelihoods_node_i)[c];
+      Vdouble* _dLikelihoods_node_i_c = &(*_dLikelihoods_node_i)[c];
+      Vdouble* _d2Likelihoods_node_i_c = &(*_d2Likelihoods_node_i)[c];
+      _likelihoods_node_i_c->resize(nbStates_);
+      _dLikelihoods_node_i_c->resize(nbStates_);
+      _d2Likelihoods_node_i_c->resize(nbStates_);
+      for (unsigned int s = 0; s < nbStates_; s++)
+      {
+        (*_likelihoods_node_i_c)[s] = 1; // All likelihoods are initialized to 1.
+        (*_dLikelihoods_node_i_c)[s] = 0; // All dLikelihoods are initialized to 0.
+        (*_d2Likelihoods_node_i_c)[s] = 0; // All d2Likelihoods are initialized to 0.
+      }
+    }
+  }
+
+  // Now initialize likelihood values and pointers:
+
+  if (node->isLeaf())
+  {
+    const Sequence* seq;
+    try
+    {
+      seq = &subSequences->getSequence(node->getName());
+    }
+    catch (SequenceNotFoundException snfe)
+    {
+      throw SequenceNotFoundException("HomogeneousTreeLikelihood::initTreelikelihoodsWithPatterns. Leaf name in tree not found in site conainer: ", (node->getName()));
+    }
+    for (unsigned int i = 0; i < nbSites; i++)
+    {
+      VVdouble* _likelihoods_node_i = &(*_likelihoods_node)[i];
+      int state = seq->getValue(i);
+      for (unsigned int c = 0; c < nbClasses_; c++)
+      {
+        Vdouble* _likelihoods_node_i_c = &(*_likelihoods_node_i)[c];
+        double test = 0.;
+        for (unsigned int s = 0; s < nbStates_; s++)
+        {
+          // Leaves likelihood are set to 1 if the char correspond to the site in the sequence,
+          // otherwise value set to 0:
+          // cout << "i=" << i << "\tc=" << c << "\ts=" << s << endl;
+          (*_likelihoods_node_i_c)[s] = model.getInitValue(s, state);
+          test += (*_likelihoods_node_i_c)[s];
+        }
+        if (test < 0.000001) std::cerr << "WARNING!!! Likelihood will be 0 for this site." << std::endl;
+      }
+    }
+  }
+  else
+  {
+    // 'node' is an internal node.
+    std::map<int, std::vector<unsigned int> >* patternLinks__node = &patternLinks_[node->getId()];
+
+    // Now initialize pattern links:
+    unsigned int nbSonNodes = node->getNumberOfSons();
+    for (unsigned int l = 0; l < nbSonNodes; l++)
+    {
+      // For each son node,
+      const Node* son = (*node)[l];
+
+      std::vector<unsigned int>* patternLinks__node_son = &(*patternLinks__node)[son->getId()];
+
+      // Initialize subtree 'l' and retrieves corresponding subSequences:
+      SitePatterns* subPatterns = initLikelihoodsWithPatterns(son, *subSequences, model);
+      (*patternLinks__node_son) = subPatterns->getIndices();
+      delete subPatterns;
+    }
+  }
+  delete subSequences;
+  return patterns;
+}
+
+/******************************************************************************/
+
---
+++ src/Bpp/Phyl/Likelihood/RTreeLikelihoodData.h
@@ -0,0 +1,311 @@
+//
+// File: RTreeLikelihoodData.h
+// Created by: Julien Dutheil
+// Created on: Sat Dec 30 14:20 2006
+// From file HomogeneousTreeLikelihood.h
+//
+
+/*
+Copyright or © or Copr. Bio++ Development Team, (November 16, 2004)
+
+This software is a computer program whose purpose is to provide classes
+for phylogenetic data analysis.
+
+This software is governed by the CeCILL  license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _RTREELIKELIHOODDATA_H_
+#define _RTREELIKELIHOODDATA_H_
+
+#include "AbstractTreeLikelihoodData.h"
+#include "../Model/SubstitutionModel.h"
+#include "../SitePatterns.h"
+
+#include <Bpp/Numeric/VectorTools.h>
+
+// From the STL:
+#include <map>
+
+namespace bpp
+{
+
+/**
+ * @brief Likelihood data structure for a node.
+ * 
+ * This class is for use with the RTreeLikelihoodData class.
+ * 
+ * Store all conditionnal likelihoods:
+ * <pre>
+ * x[i][c][s]
+ *   |---------> Site i
+ *      |------> Class c
+ *         |---> Ancestral state s
+ * </pre> 
+ * We call this the <i>likelihood array</i> for each node.
+ * In the same way, we store first and second order derivatives.
+ *
+ * @see RTreeLikelihoodData
+ */
+class RTreeLikelihoodNodeData :
+  public virtual TreeLikelihoodNodeData
+{
+  private:
+    mutable VVVdouble nodeLikelihoods_;
+    mutable VVVdouble nodeDLikelihoods_;
+    mutable VVVdouble nodeD2Likelihoods_;
+    const Node* node_;
+
+  public:
+    RTreeLikelihoodNodeData() : nodeLikelihoods_(), nodeDLikelihoods_(), nodeD2Likelihoods_(), node_(0) {}
+    
+    RTreeLikelihoodNodeData(const RTreeLikelihoodNodeData& data) :
+      nodeLikelihoods_(data.nodeLikelihoods_),
+      nodeDLikelihoods_(data.nodeDLikelihoods_),
+      nodeD2Likelihoods_(data.nodeD2Likelihoods_),
+      node_(data.node_)
+    {}
+    
+    RTreeLikelihoodNodeData& operator=(const RTreeLikelihoodNodeData& data)
+    {
+      nodeLikelihoods_   = data.nodeLikelihoods_;
+      nodeDLikelihoods_  = data.nodeDLikelihoods_;
+      nodeD2Likelihoods_ = data.nodeD2Likelihoods_;
+      node_              = data.node_;
+      return *this;
+    }
+ 
+#ifndef NO_VIRTUAL_COV
+    RTreeLikelihoodNodeData*
+#else
+    Clonable*
+#endif
+    clone() const
+    {
+      return new RTreeLikelihoodNodeData(*this);
+    }
+
+  public:
+    const Node* getNode() const { return node_; }
+    void setNode(const Node* node) { node_ = node; }
+
+    VVVdouble& getLikelihoodArray() { return nodeLikelihoods_; }
+    const VVVdouble& getLikelihoodArray() const { return nodeLikelihoods_; }
+    
+    VVVdouble& getDLikelihoodArray() { return nodeDLikelihoods_; }
+    const VVVdouble& getDLikelihoodArray() const { return nodeDLikelihoods_; }
+
+    VVVdouble& getD2LikelihoodArray() { return nodeD2Likelihoods_; }
+    const VVVdouble& getD2LikelihoodArray() const { return nodeD2Likelihoods_; }
+};
+
+/**
+ * @brief discrete Rate Across Sites, (simple) Recursive likelihood data structure.
+ */
+class RTreeLikelihoodData :
+  public virtual AbstractTreeLikelihoodData
+{
+  private:
+    /**
+     * @brief This contains all likelihood values used for computation.
+     *
+     */
+    mutable std::map<int, RTreeLikelihoodNodeData> nodeData_;
+      
+    /**
+     * @brief This map defines the pattern network.
+     *
+     * Let n1 be the id of a node in the tree, and n11 and n12 the ids of its sons.
+     * Providing the likelihood array is known for nodes n11 and n12,
+     * the likelihood array for node n1 and site <i>i</i> (_likelihood[n1][i]) must be computed  
+     * using arrays patternLinks_[n1][n11][i] and patternLinks_[n1][n12][i].
+     * This network is intialized once for all in the constructor of this class.
+     *
+     * The double map contains the position of the site to use (second dimension)
+     * of the likelihoods array.
+     */
+    mutable std::map<int, std::map<int, std::vector<unsigned int> > > patternLinks_;
+    SiteContainer* shrunkData_;
+    unsigned int nbSites_; 
+    unsigned int nbStates_;
+    unsigned int nbClasses_;
+    unsigned int nbDistinctSites_; 
+    bool usePatterns_;
+
+  public:
+    RTreeLikelihoodData(const TreeTemplate<Node>* tree, unsigned int nbClasses, bool usePatterns = true) :
+      AbstractTreeLikelihoodData(tree),
+      nodeData_(), patternLinks_(), shrunkData_(0), nbSites_(0), nbStates_(0),
+      nbClasses_(nbClasses), nbDistinctSites_(0), usePatterns_(usePatterns)
+    {}
+
+    RTreeLikelihoodData(const RTreeLikelihoodData& data):
+      AbstractTreeLikelihoodData(data),
+      nodeData_(data.nodeData_),
+      patternLinks_(data.patternLinks_),
+      shrunkData_(0),
+      nbSites_(data.nbSites_), nbStates_(data.nbStates_),
+      nbClasses_(data.nbClasses_), nbDistinctSites_(data.nbDistinctSites_),
+      usePatterns_(data.usePatterns_)
+    {
+      if (data.shrunkData_)
+        shrunkData_      = dynamic_cast<SiteContainer *>(data.shrunkData_->clone());
+    }
+
+    RTreeLikelihoodData& operator=(const RTreeLikelihoodData & data)
+    {
+      AbstractTreeLikelihoodData::operator=(data);
+      nodeData_          = data.nodeData_;
+      patternLinks_      = data.patternLinks_;
+      nbSites_           = data.nbSites_;
+      nbStates_          = data.nbStates_;
+      nbClasses_         = data.nbClasses_;
+      nbDistinctSites_   = data.nbDistinctSites_;
+      if (shrunkData_) delete shrunkData_;
+      if (data.shrunkData_)
+        shrunkData_      = dynamic_cast<SiteContainer*>(data.shrunkData_->clone());
+      else
+        shrunkData_      = 0;
+      usePatterns_       = data.usePatterns_;
+      return *this;
+    }
+
+    virtual ~RTreeLikelihoodData() { delete shrunkData_; }
+
+    RTreeLikelihoodData* clone() const { return new RTreeLikelihoodData(*this); }
+
+  public:
+    /**
+     * @brief Set the tree associated to the data.
+     *
+     * All node data will be actualized accordingly by calling the setNode() method on the corresponding nodes.
+     * @warning: the old tree and the new tree must be two clones! And particularly, they have to share the
+     * same topology and nodes id.
+     *
+     * @param tree The tree to be associated to this data.
+     */
+    void setTree(const TreeTemplate<Node>* tree)
+    { 
+      tree_ = tree;
+      for (std::map<int, RTreeLikelihoodNodeData>::iterator it = nodeData_.begin(); it != nodeData_.end(); it++)
+      {
+        int id = it->second.getNode()->getId();
+        it->second.setNode(tree_->getNode(id));
+      }
+    }
+ 
+    RTreeLikelihoodNodeData& getNodeData(int nodeId)
+    { 
+      return nodeData_[nodeId];
+    }
+    const RTreeLikelihoodNodeData& getNodeData(int nodeId) const
+    { 
+      return nodeData_[nodeId];
+    }
+    unsigned int getArrayPosition(int parentId, int sonId, unsigned int currentPosition) const
+    {
+      return patternLinks_[parentId][sonId][currentPosition];
+    }
+    unsigned int getRootArrayPosition(unsigned int currentPosition) const
+    {
+      return rootPatternLinks_[currentPosition];
+    }
+    const std::vector<unsigned int>& getArrayPositions(int parentId, int sonId) const
+    {
+      return patternLinks_[parentId][sonId];
+    }
+    std::vector<unsigned int>& getArrayPositions(int parentId, int sonId)
+    {
+      return patternLinks_[parentId][sonId];
+    }
+    unsigned int getArrayPosition(int parentId, int sonId, unsigned int currentPosition)
+    {
+      return patternLinks_[parentId][sonId][currentPosition];
+    }
+
+    VVVdouble& getLikelihoodArray(int nodeId)
+    {
+      return nodeData_[nodeId].getLikelihoodArray();
+    }
+    
+    VVVdouble& getDLikelihoodArray(int nodeId)
+    {
+      return nodeData_[nodeId].getDLikelihoodArray();
+    }
+    
+    VVVdouble& getD2LikelihoodArray(int nodeId)
+    {
+      return nodeData_[nodeId].getD2LikelihoodArray();
+    }
+
+    unsigned int getNumberOfDistinctSites() const { return nbDistinctSites_; }
+    unsigned int getNumberOfSites() const { return nbSites_; }
+    unsigned int getNumberOfStates() const { return nbStates_; }
+    unsigned int getNumberOfClasses() const { return nbClasses_; }
+    
+    void initLikelihoods(const SiteContainer& sites, const SubstitutionModel& model) throw (Exception);
+
+  protected:
+    /**
+     * @brief This method initializes the leaves according to a sequence file.
+     * likelihood is set to 1 for the state corresponding to the sequence site,
+     * otherwise it is set to 0.
+     *
+     * All likelihood arrays at each nodes are initialized according to alphabet
+     * size and sequences length, and filled with 1.
+     *
+     * NB: This method is recursive.
+     *
+     * @param node      The node defining the subtree to analyse.
+     * @param sequences The data to be used for initialization.
+     * @param model     The model to use.
+     */
+    virtual void initLikelihoods(const Node* node, const SiteContainer& sequences, const SubstitutionModel& model) throw (Exception);
+
+    /**
+     * @brief This method initializes the leaves according to a sequence file.
+     *
+     * likelihood is set to 1 for the state corresponding to the sequence site,
+     * otherwise it is set to 0.
+     *
+     * All likelihood arrays at each nodes are initialized according to alphabet
+     * size and sequences length, and filled with 1.
+     *
+     * NB: This method is recursive.
+     *
+     * @param node      The node defining the subtree to analyse.
+     * @param sequences The data to be used for initialization.
+     * @param model     The model to use.
+     * @return The shrunk sub-dataset + indices for the subtree defined by <i>node</i>.
+     */
+    virtual SitePatterns* initLikelihoodsWithPatterns(const Node* node, const SiteContainer& sequences, const SubstitutionModel& model) throw (Exception);
+  
+};
+
+} //end of namespace bpp.
+
+#endif //_RTREELIKELIHOODDATA_H_
+
