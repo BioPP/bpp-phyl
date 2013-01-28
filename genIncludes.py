3cedf8eba919e83d3dc659b147caadfbe684a0fb
Julien Y. Dutheil <jy.dutheil@gmail.com>
2013-01-28 11:10:20 +0100

    Switched to automatic .all files generation.

--- genIncludes.py
+++ genIncludes.py
@@ -2,9 +2,10 @@
 
 import os;
 from os.path import *;
+import sys;
 
-for root, dirs, files in os.walk('src/Bpp'):
-    print "Creating generic include file " + str(root) + ".all"
+for root, dirs, files in os.walk(sys.argv[1]):
+    print "-- Creating generic include file: " + str(root) + ".all"
     f = open(str(root) + ".all", 'w')
     head, tail = split(root)
     files.sort()
