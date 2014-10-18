383c77e1bbb34a2ec6d9e7578ece644c52ee427d
Julien Y. Dutheil <jy.dutheil@gmail.com>
2014-10-18 22:42:56 +0200

    Test of rootAt in separate file.

--- test/test_tree.cpp
+++ test/test_tree.cpp
@@ -264,27 +264,5 @@ int main() {
     cout << vecNode[i]->getId() << endl;
   }
  
-
-  //Testing rerooting:
-  //Get a random topology:
-  auto_ptr< TreeTemplate<Node> > tr(TreeTemplateTools::getRandomTree(leaves, true));
-  //Set random branch lengths:
-  vector<Node*> nodes = tr->getNodes();
-  for (size_t i = 0; i < nodes.size(); ++i) {
-    if (nodes[i]->hasFather())
-      nodes[i]->setDistanceToFather(RandomTools::giveRandomNumberBetweenZeroAndEntry(1.));
-  }
-  double totalLen = TreeTools::getTotalLength(*tr.get(), tr->getRootId(), false);
-  ApplicationTools::displayResult("Total length", totalLen);
-  for (unsigned int i = 0; i < 100; ++i) {
-    size_t pos = RandomTools::giveIntRandomNumberBetweenZeroAndEntry<size_t>(100);
-    tr->rootAt(nodes[pos]);
-    double l = TreeTools::getTotalLength(*tr.get(), tr->getRootId(), false);
-    if ((l - totalLen) / totalLen > 0.00000001) {
-      cerr << "Error, rerooting gave incorrect branch lengths :(: " << l << " vs. " << totalLen << endl;
-      return 1;
-    }
-  }
-
   return 0;
 }
