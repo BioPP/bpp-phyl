6de13ffb2b9de9ca0211a6bc2e16b252ff13a3bd
Laurent Gueguen <laurent.gueguen@univ-lyon1.fr>
2016-07-24 08:20:44 +0200

    Equi probable model

---
+++ src/Bpp/Phyl/Model/EquiprobableSubstitutionModel.cpp
@@ -0,0 +1,196 @@
+//
+// File: EquiprobableSubstitutionModel.cpp
+// Created by: Laurent Guéguen
+// Created on: mercredi 13 juillet 2016, à 08h 55
+//
+
+/*
+  Copyright or © or Copr. Bio++ Development Team, (November 16, 2004)
+
+  This software is a computer program whose purpose is to provide classes
+  for phylogenetic data analysis.
+
+  This software is governed by the CeCILL  license under French law and
+  abiding by the rules of distribution of free software.  You can  use, 
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info". 
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability. 
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or 
+  data to be ensured and,  more generally, to use and operate it in the 
+  same conditions as regards security. 
+
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "EquiprobableSubstitutionModel.h"
+
+//From bpp-seq:
+#include <Bpp/Seq/Container/SequenceContainerTools.h>
+
+using namespace bpp;
+
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+/******************************************************************************/
+
+EquiprobableSubstitutionModel::EquiprobableSubstitutionModel(const Alphabet* alpha) :
+  AbstractParameterAliasable("Equi."),
+  AbstractReversibleSubstitutionModel(alpha, new CanonicalStateMap(alpha, false), "Equi."),
+  exp_(), p_(size_, size_), freqSet_(0)
+{
+  freqSet_ = new FixedFrequenciesSet(getStateMap().clone(), freq_);
+  updateMatrices();
+}
+
+EquiprobableSubstitutionModel::EquiprobableSubstitutionModel(const Alphabet* alpha, FrequenciesSet* freqSet, bool initFreqs) :
+  AbstractParameterAliasable("Equi+F."),
+  AbstractReversibleSubstitutionModel(alpha, new CanonicalStateMap(alpha, false), "Equi+F."),
+  exp_(), p_(size_, size_), freqSet_(freqSet)
+{
+  freqSet_->setNamespace("Equi+F."+freqSet_->getNamespace());
+  if (initFreqs) freqSet_->setFrequencies(freq_);
+  else freq_ = freqSet_->getFrequencies();
+  addParameters_(freqSet_->getParameters());
+  updateMatrices();  
+}
+
+
+/******************************************************************************/
+	
+void EquiprobableSubstitutionModel::updateMatrices()
+{
+  // Frequencies:
+  for (unsigned int i = 0; i < size_; i++) freq_[i] = 1. / (float)size_;
+
+  // Generator:
+  for (unsigned int i = 0; i < size_; i++)
+  {
+    for (unsigned int j = 0; j < size_; j++)
+    {
+      generator_(i, j) = (i == j) ? -1. : 1./(float)(size_-1);
+      exchangeability_(i, j) = generator_(i, j) * (float)size_;
+    }
+  }
+
+  // Eigen values:
+  eigenValues_[0] = 0;
+  for (unsigned int i = 1; i < size_; i++) eigenValues_[i] = -(float)size_ / (float)(size_-1);
+	
+  // Eigen vectors:
+  for (unsigned int i = 0; i < size_; i++) leftEigenVectors_(0,i) = 1./(float)size_;
+  for (unsigned int i = 1; i < size_; i++) 
+    for (unsigned int j = 0; j < size_; j++)
+      leftEigenVectors_(i,j) = -1./(float)size_;
+  for (unsigned int i = 0; i < (size_-1); i++) leftEigenVectors_((size_-1)-i,i) = (float)(size_-1)/(float)size_;
+
+  for (unsigned int i = 0; i < size_; i++) rightEigenVectors_(i,0) = 1.;
+  for (unsigned int i = 1; i < size_; i++) rightEigenVectors_((size_-1),i) = -1.;
+  for (unsigned int i = 0; i < (size_-1); i++) 
+    for (unsigned int j = 1; j < size_; j++)
+      rightEigenVectors_(i,j) = 0.;
+  for (unsigned int i = 1; i < size_; i++) rightEigenVectors_((size_-1)-i,i) = 1.;
+
+}
+	
+/******************************************************************************/
+
+double EquiprobableSubstitutionModel::Pij_t(size_t i, size_t j, double d) const
+{
+  if(i == j) return 1./(float)size_ + (float)(size_-1)/(float)size_ * exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+  else       return 1./(float)size_ -  1./(float)size_ * exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+}
+
+/******************************************************************************/
+
+double EquiprobableSubstitutionModel::dPij_dt(size_t i, size_t j, double d) const
+{
+  if(i == j) return -  rate_ *        exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+  else       return  rate_ * 1./(float)(size_-1) * exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+}
+
+/******************************************************************************/
+
+double EquiprobableSubstitutionModel::d2Pij_dt2(size_t i, size_t j, double d) const
+{
+  if(i == j) return    rate_ *  rate_ * (float)size_/(float)(size_-1)  * exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+  else       return -  rate_ *  rate_ * (float)size_/pow((float)(size_-1),2) * exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+}
+
+/******************************************************************************/
+
+const Matrix<double>& EquiprobableSubstitutionModel::getPij_t(double d) const
+{
+  exp_ = exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+  for(unsigned int i = 0; i < size_; i++)
+  {
+    for(unsigned int j = 0; j < size_; j++)
+    {
+      p_(i,j) = (i==j) ? 1./(float)size_ + (float)(size_-1)/(float)size_ * exp_ : 1./(float)size_ - 1./(float)size_ * exp_;
+    }
+  }
+  return p_;
+}
+
+const Matrix<double>& EquiprobableSubstitutionModel::getdPij_dt(double d) const
+{
+  exp_ = exp(-  rate_ * (float)size_/(float)(size_-1) * d);
+  for(unsigned int i = 0; i < size_; i++)
+  {
+    for(unsigned int j = 0; j < size_; j++)
+    {
+      p_(i,j) =  rate_ * ((i==j) ? - exp_ : 1./(float)(size_-1) * exp_);
+    }
+  }
+  return p_;
+}
+
+const Matrix<double>& EquiprobableSubstitutionModel::getd2Pij_dt2(double d) const
+{
+  exp_ = exp( rate_ * - (float)size_/(float)(size_-1) * d);
+  for(unsigned int i = 0; i < size_; i++)
+  {
+    for(unsigned int j = 0; j < size_; j++)
+    {
+      p_(i,j) =  rate_ *  rate_ * ((i==j) ? (float)size_/(float)(size_-1) * exp_ : - (float)size_/pow((float)(size_-1),2) * exp_);
+    }
+  }
+  return p_;
+}
+
+/******************************************************************************/
+
+void EquiprobableSubstitutionModel::setFreqFromData(const SequenceContainer& data, double pseudoCount)
+{
+  map<int, int> counts;
+  SequenceContainerTools::getCounts(data, counts);
+  double t = 0;
+  for (int i = 0; i < static_cast<int>(size_); i++)
+  {
+    t += (counts[i] + pseudoCount);
+  }
+  for (size_t i = 0; i < size_; ++i) freq_[i] = (static_cast<double>(counts[static_cast<int>(i)]) + pseudoCount) / t;
+  freqSet_->setFrequencies(freq_);
+  //Update parameters and re-compute generator and eigen values:
+  matchParametersValues(freqSet_->getParameters());
+}
+
+/******************************************************************************/
+
---
+++ src/Bpp/Phyl/Model/EquiprobableSubstitutionModel.h
@@ -0,0 +1,235 @@
+//
+// File: EquiprobableSubstitutionModel.h
+// Created by: Laurent Guéguen
+// Created on: mercredi 13 juillet 2016, à 08h 49
+//
+
+/*
+  Copyright or © or Copr. Bio++ Development Team, (November 16, 2004)
+
+  This software is a computer program whose purpose is to provide classes
+  for phylogenetic data analysis.
+
+  This software is governed by the CeCILL  license under French law and
+  abiding by the rules of distribution of free software.  You can  use, 
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info". 
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability. 
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or 
+  data to be ensured and,  more generally, to use and operate it in the 
+  same conditions as regards security. 
+
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _EQUIPROBABLE_SUBSTITUTION_MODEL_H_
+#define _EQUIPROBABLE_SUBSTITUTION_MODEL_H_
+
+#include <Bpp/Seq/Alphabet/Alphabet.h>
+#include "AbstractSubstitutionModel.h"
+#include "FrequenciesSet/FrequenciesSet.h"
+
+namespace bpp
+{
+
+  /**
+   * @brief The EquiprobableSubstitutionModel substitution model for any kind of
+   * alphabet. Jukes-Cantor models are specific case of this model,
+   * applied to nucleotides or proteins.
+   *
+   * If the number of states is \f$N\f$.
+   *
+   * All rates equal:
+   * \f[
+   * \begin{pmatrix}
+   * \ddots & r      & \ldots & r \\ 
+   * r      & \ddots & \ddots & \vdots \\ 
+   * \vdots & \ddots & \ddots & r \\
+   * r      & \ldots & r      & \ddots \\ 
+   * \end{pmatrix}
+   * \f]
+   * \f[
+   * \pi = diag\left(\frac{1}{N}, \ldots, \frac{1}{N}\right)
+   * \f]
+   * Normalization: \f$r\f$ is set so that \f$\sum_i Q_{i,i}\pi_i = -1\f$:
+   * \f[
+   * S = \begin{pmatrix}
+   * -N           & \frac{N}{N-1} & \ldots        & \frac{N}{N-1} \\ 
+   * \frac{N}{N-1} &           -N & \ddots        & \vdots \\ 
+   * \vdots        & \ddots        & \ddots        & \frac{N}{N-1} \\
+   * \frac{N}{N-1} & \ldots        & \frac{N}{N-1} & -N \\ 
+   * \end{pmatrix}
+   * \f]
+   * The normalized generator is obtained by taking the dot product of \f$S\f$ and \f$pi\f$:
+   * \f[
+   * Q = S . \pi = \begin{pmatrix}
+   * -1 & \frac{1}{N-1}     & \ldots & \frac{1}{N-1} \\ 
+   * \frac{1}{N-1} & -1     & \ddots & \vdots \\ 
+   * \vdots       & \ddots & \ddots & \frac{1}{N-1} \\ 
+   * \frac{1}{N-1} & \ldots & \frac{1}{N-1} & -1 \\ 
+   * \end{pmatrix}
+   * \f]
+   *
+   * The eigen values are \f$\left(0, -\frac{N}{N-1}, \ldots, -\frac{N}{N-1}\right)\f$, 
+   * the left eigen vectors are, by row:
+   * \f[
+   * U = \begin{pmatrix}
+   *   \frac{1}{N} &        \ldots &  \frac{1}{N} &   \frac{1}{N} &  \frac{1}{N} \\
+   *  -\frac{1}{N} &        \ldots & -\frac{1}{N} &  \frac{N-1}{N} & -\frac{1}{N} \\
+   *         \vdots &        \ddots & \frac{N-1}{N} & -\frac{1}{N}  & -\frac{1}{N} \\
+   *  -\frac{1}{N} &        \ddots &        \ddots &         \vdots &        \vdots \\
+   *  \frac{N-1}{N} & -\frac{1}{N} &        \ldots &  -\frac{1}{N} & -\frac{1}{N} \\
+   * \end{pmatrix}
+   * \f]
+   * and the right eigen vectors are, by column:
+   * \f[
+   * U^-1 = \begin{pmatrix}
+   *      1 &      0 &  \ldots &      0 &  1 \\
+   * \vdots & \vdots &  \ddots & \ddots &  0 \\
+   *      1 &      0 &       1 & \ddots & \vdots \\
+   *      1 &      1 &       0 & \ldots &  0 \\
+   *      1 &     -1 &      -1 & \ldots & -1 \\
+   * \end{pmatrix}
+   * \f]
+   *
+   * In addition, a rate_ factor defines the mean rate of the model.
+   *
+   * The probabilities of changes are computed analytically using the formulas:
+   * \f[
+   * P_{i,j}(t) = \begin{cases}
+   * \frac{1}{N} + \frac{N-1}{N}e^{- rate\_ * \frac{N}{N-1}t}& \text{if $i=j$}, \\
+   * \frac{1}{N} - \frac{1}{N}e^{- rate\_ * \frac{N}{N-1}t} & \text{otherwise}.
+   * \end{cases}
+   * \f]
+   *
+   * First and second order derivatives are also computed analytically using the formulas:
+   * \f[
+   * \frac{\partial P_{i,j}(t)}{\partial t} = rate\_ * \begin{cases}
+   * -e^{- rate\_ * \frac{N}{N-1}t}           & \text{if $i=j$}, \\
+   * \frac{1}{N-1}e^{- rate\_ * \frac{N}{N-1}t} & \text{otherwise}.
+   * \end{cases}
+   * \f]
+   * \f[
+   * \frac{\partial^2 P_{i,j}(t)}{\partial t^2} =  rate\_^2 * \begin{cases}
+   * \frac{N}{N-1}e^{- rate\_ * \frac{N}{N-1}t}  & \text{if $i=j$}, \\
+   * -\frac{N}{(N-1)^2}e^{- rate\_ * \frac{N}{N-1}t} & \text{otherwise}.
+   * \end{cases}
+   * \f]
+   *
+   */
+  class EquiprobableSubstitutionModel:
+    public AbstractReversibleSubstitutionModel
+  {
+  private:
+    mutable double exp_;
+    mutable RowMatrix<double> p_;
+    FrequenciesSet* freqSet_;
+
+  public:
+    /**
+     * @brief Build a simple equiprobable model, with original equilibrium frequencies.
+     *
+     * @param alpha An alphabet.
+     */
+    EquiprobableSubstitutionModel(const Alphabet* alpha);
+
+    /**
+     * @brief Build an equiprobable model with special equilibrium frequencies.
+     *
+     * @param alpha An alphabet.
+     * @param freqSet A pointer toward a frequencies set, which will be owned by this instance.
+     * @param initFreqs Tell if the frequency set should be initialized with the original JTT92 values.
+     * Otherwise, the values of the set will be used.
+     */
+    EquiprobableSubstitutionModel(const Alphabet* alpha, FrequenciesSet* freqSet, bool initFreqs = false);
+
+    EquiprobableSubstitutionModel(const EquiprobableSubstitutionModel& model) :
+      AbstractParameterAliasable(model),
+      AbstractReversibleSubstitutionModel(model),
+      exp_(model.exp_),
+      p_(model.p_),
+      freqSet_(dynamic_cast<FrequenciesSet*>(model.freqSet_->clone()))
+    {}
+
+    EquiprobableSubstitutionModel& operator=(const EquiprobableSubstitutionModel& model)
+    {
+      AbstractParameterAliasable::operator=(model);
+      AbstractReversibleSubstitutionModel::operator=(model);
+      exp_ = model.exp_;
+      p_   = model.p_;
+      if (freqSet_) delete freqSet_;
+      freqSet_ = dynamic_cast<FrequenciesSet*>(model.freqSet_->clone());
+      return *this;
+    }
+
+    virtual ~EquiprobableSubstitutionModel() { delete freqSet_; }
+	
+    EquiprobableSubstitutionModel* clone() const { return new EquiprobableSubstitutionModel(*this); }
+
+  public:
+    double Pij_t    (size_t i, size_t j, double d) const;
+    double dPij_dt  (size_t i, size_t j, double d) const;
+    double d2Pij_dt2(size_t i, size_t j, double d) const;
+    const Matrix<double>& getPij_t    (double d) const;
+    const Matrix<double>& getdPij_dt  (double d) const;
+    const Matrix<double>& getd2Pij_dt2(double d) const;
+
+    size_t getNumberOfStates() const { return size_; };
+
+    std::string getName() const 
+    { 
+      if (freqSet_->getNamespace().find("+F.") != std::string::npos)
+        return "Equi+F"; 
+      else 
+        return "Equi"; 
+    }
+	
+    void fireParameterChanged(const ParameterList& parameters)
+    {
+      freqSet_->matchParametersValues(parameters);
+      freq_ = freqSet_->getFrequencies();
+      AbstractReversibleSubstitutionModel::fireParameterChanged(parameters);
+    }
+
+    void setFrequenciesSet(const FrequenciesSet& freqSet)
+    {
+      delete freqSet_;
+      freqSet_ = dynamic_cast<FrequenciesSet*>(freqSet.clone());
+      resetParameters_();
+      addParameters_(freqSet_->getParameters());
+    }
+
+    const FrequenciesSet* getFrequenciesSet() const { return freqSet_; }
+
+    void setFreqFromData(const SequenceContainer& data, double pseudoCount = 0);
+
+  protected:
+    /**
+     * In the case of the model of Jukes & Cantor, this method is useless since
+     * the generator is fixed! No matrice can be changed... This method is only
+     * used in the constructor of the class.
+     */
+    void updateMatrices();
+
+    
+  };
+
+} //end of namespace bpp.
+
+#endif	//_EQUIPROBABLE_SUBSTITUTION_MODEL_H_
+
