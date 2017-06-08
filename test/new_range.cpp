44398263f2548f1d95fdd3e2302dbf5175dfa6ac
François Gindraud <francois.gindraud@univ-lyon1.fr>
2017-06-08 16:39:43 +0200

    Clean test file headers

--- test/new_range.cpp
+++ test/new_range.cpp
@@ -2,28 +2,28 @@
 // File: new_range.cpp
 // Authors:
 //   Francois Gindraud (2017)
-// Created: 2017-05-05
-// Last modified: 2017-05-05
+// Created: 2017-05-05 00:00:00
+// Last modified: 2017-06-08
 //
 
 /*
   Copyright or © or Copr. Bio++ Development Team, (November 17, 2004)
-
+  
   This software is a computer program whose purpose is to provide classes
   for numerical calculus. This file is part of the Bio++ project.
-
+  
   This software is governed by the CeCILL license under French law and
   abiding by the rules of distribution of free software. You can use,
   modify and/ or redistribute the software under the terms of the CeCILL
   license as circulated by CEA, CNRS and INRIA at the following URL
   "http://www.cecill.info".
-
+  
   As a counterpart to the access to the source code and rights to copy,
   modify and redistribute granted by the license, users are provided only
   with a limited warranty and the software's author, the holder of the
   economic rights, and the successive licensors have only limited
   liability.
-
+  
   In this respect, the user's attention is drawn to the risks associated
   with loading, using, modifying and/or developing or reproducing the
   software by the user in light of its specific status of free software,
@@ -34,7 +34,7 @@
   requirements in conditions enabling the security of their systems and/or
   data to be ensured and, more generally, to use and operate it in the
   same conditions as regards security.
-
+  
   The fact that you are presently reading this means that you have had
   knowledge of the CeCILL license and that you accept its terms.
 */
