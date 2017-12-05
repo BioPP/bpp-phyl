486b5de9dcf811920985003eed3e7f8f4cdbda26
François Gindraud <francois.gindraud@univ-lyon1.fr>
2017-12-05 18:10:37 +0100

    Removed unused topology & frozenptr stuff

--- test/new_phyl_dataflow.cpp
+++ test/new_phyl_dataflow.cpp
@@ -305,7 +305,7 @@ TEST_CASE("df")
     std::ofstream fd("df_debug");
     // bpp::DF::debugDag(fd, logLikNode, bpp::DF::DebugOptions::DetailedNodeInfo);
     bpp::debugDag(fd, likFunc.getAllNamedNodes("f"), bpp::DF::DebugOptions::DetailedNodeInfo);
-    // bpp::Topology::debugTree(fd, treeData.topology);
+    //bpp::debugTree(fd, treeView);
   }
   do_param_changes_multiple_times(likFunc, "df_param_model_change", c.paramModel1, c.paramModel2);
   do_param_changes_multiple_times(likFunc, "df_param_brlen_change", c.paramBrLen1, c.paramBrLen2);
