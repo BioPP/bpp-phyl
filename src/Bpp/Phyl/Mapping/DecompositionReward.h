139c910272adab47d425f9c136b391d5669ba366
Julien Y. Dutheil <dutheil@evolbio.mpg.de>
2018-02-26 09:12:05 +0100

    Removed dynamic exception.

--- src/Bpp/Phyl/Mapping/DecompositionReward.h
+++ src/Bpp/Phyl/Mapping/DecompositionReward.h
@@ -99,7 +99,6 @@ namespace bpp
 
     Matrix<double>* getAllRewards(double length) const;
     
-<<<<<<< HEAD
     /**
      * @brief Set the substitution model.
      *
@@ -120,29 +119,6 @@ namespace bpp
   private:
     void fillBMatrice_();
   };
-=======
-  /**
-   * @brief Set the substitution model.
-   *
-   * @param model A pointer toward the substitution model to use. Only
-   * reversible models are currently supported. Setting a
-   * non-reversible model will throw an exception.
-   *
-   */
-  void setSubstitutionModel(const SubstitutionModel* model);
-
-protected:
-  void computeRewards_(double length) const;
-  void jFunction_(const std::vector<double>& lambda, double t, RowMatrix<double>& result) const;
-  void alphabetIndexHasChanged();
-
-private:
-  void resetStates_();
-  void computeBMatrice_();
-  void computeEigen_();
-  void computeProducts_();
-};
->>>>>>> 36ee90f0cbb9c7f97c05f79cbda9a5f0f7c509d2
 
 } //end of namespace bpp.
 
