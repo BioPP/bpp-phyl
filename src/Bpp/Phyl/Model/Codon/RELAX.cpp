a609ce919378aa8502a96943426116f62267c9d8
halabikeren <halabikeren@gmail.com>
2019-05-06 11:24:40 +0300

    Added implementation of RELAX model according to Wertheim et al. 2014

---
+++ src/Bpp/Phyl/Model/Codon/RELAX.cpp
@@ -0,0 +1,318 @@
+//
+// File: RELAX.cpp
+// Created by:  Keren Halabi
+// Created on: August 2018
+//
+
+/*
+   Copyright or © or Copr. Bio++ Development Team, (November 16, 2004)
+   This software is a computer program whose purpose is to provide classes
+   for phylogenetic data analysis.
+
+   This software is governed by the CeCILL  license under French law and
+   abiding by the rules of distribution of free software.  You can  use,
+   modify and/ or redistribute the software under the terms of the CeCILL
+   license as circulated by CEA, CNRS and INRIA at the following URL
+   "http://www.cecill.info".
+
+   As a counterpart to the access to the source code and  rights to copy,
+   modify and redistribute granted by the license, users are provided only
+   with a limited warranty  and the software's author,  the holder of the
+   economic rights,  and the successive licensors  have only  limited
+   liability.
+
+   In this respect, the user's attention is drawn to the risks associated
+   with loading,  using,  modifying and/or developing or reproducing the
+   software by the user in light of its specific status of free software,
+   that may mean  that it is complicated to manipulate,  and  that  also
+   therefore means  that it is reserved for developers  and  experienced
+   professionals having in-depth computer knowledge. Users are therefore
+   encouraged to load and test the software's suitability as regards their
+   requirements in conditions enabling the security of their systems and/or
+   data to be ensured and,  more generally, to use and operate it in the
+   same conditions as regards security.
+
+   The fact that you are presently reading this means that you have had
+   knowledge of the CeCILL license and that you accept its terms.
+ */
+
+#include "RELAX.h"
+#include <Bpp/Phyl/Model/Codon/YN98.h>
+#include <Bpp/Phyl/Model/MixtureOfASubstitutionModel.h>
+#include <math.h>                     /* pow */
+
+#include <Bpp/Numeric/NumConstants.h>
+#include <Bpp/Numeric/Prob/SimpleDiscreteDistribution.h>
+#include <Bpp/Numeric/AutoParameter.h>
+
+using namespace bpp;
+
+using namespace std;
+
+/******************************************************************************/
+
+RELAX::RELAX(const GeneticCode* gc, FrequenciesSet* codonFreqs) :
+  YNGP_M("RELAX."), // RELAX currenly inherits from YNGP_M as well, since it uses kappa and instead of the 5 GTR parameters
+  adjustableParToStatus_()
+{
+
+  // set the initial omegas distribution
+  vector<double> omega_initials, omega_frequencies_initials;
+  omega_initials.push_back(0.5); omega_initials.push_back(1); omega_initials.push_back(2);
+  omega_frequencies_initials.push_back(0.333333); omega_frequencies_initials.push_back(0.333333); omega_frequencies_initials.push_back(0.333334);
+
+  SimpleDiscreteDistribution* psdd = new SimpleDiscreteDistribution(omega_initials, omega_frequencies_initials);
+
+  map<string, DiscreteDistribution*> mpdd;
+  mpdd["omega"] = psdd;
+
+  // build the submodel as a basic Yang Nielsen model (with kappa instead of 5 GTR nucleotide substituion rate parameters) 
+  unique_ptr<YN98> yn98(new YN98(gc, codonFreqs));
+
+  // initialize the site model with the initial omegas distribution
+  pmixmodel_.reset(new MixtureOfASubstitutionModel(gc->getSourceAlphabet(), yn98.get(), mpdd));
+  delete psdd; // delete the initial omegas distibution, that is already embedded in the mixture model
+
+  // mapping the parameters
+  ParameterList pl = pmixmodel_->getParameters();
+  for (size_t i = 0; i < pl.size(); i++)
+  {
+    lParPmodel_.addParameter(Parameter(pl[i])); // add the parameter to the biblio wrapper instance - see Laurent's response in https://groups.google.com/forum/#!searchin/biopp-help-forum/likelihood$20ratio$20test|sort:date/biopp-help-forum/lH8MYit_Mr8/2CBND79B11YJ
+  }
+
+  // v consists of 9 shared theta parameters, that are used for the F3X4 estimation of codon frequencies  
+  vector<std::string> v = dynamic_cast<YN98*>(pmixmodel_->getNModel(0))->getFrequenciesSet()->getParameters().getParameterNames();
+
+  for (size_t i = 0; i < v.size(); i++)
+  {
+    mapParNamesFromPmodel_[v[i]] = v[i].substr(5);
+  }
+
+  // map the parameters of RELAX to the parameters of the sub-models
+  mapParNamesFromPmodel_["YN98.kappa"] = "kappa";
+  mapParNamesFromPmodel_["YN98.omega_Simple.V1"] = "p";           // omega0=p*omega1 (p is re-parameterization of omega0)
+  mapParNamesFromPmodel_["YN98.omega_Simple.V2"] = "omega1";     
+  mapParNamesFromPmodel_["YN98.omega_Simple.theta1"] = "theta1";  // frequency of omega1 (denoted in YNGP_M2's documentation as p0)
+  mapParNamesFromPmodel_["YN98.omega_Simple.V3"] = "omega2";
+  mapParNamesFromPmodel_["YN98.omega_Simple.theta2"] = "theta2";  // theta2 = (p1/(p1+p2))
+  /* codon frequencies parameterization using F3X4: for each _Full.theta, corresponding to a a codon position over {0,1,2}:
+  getFreq_(0) = theta1 * (1. - theta);
+  getFreq_(1) = (1 - theta2) * theta;
+  getFreq_(2) = theta2 * theta;
+  getFreq_(3) = (1 - theta1) * (1. - theta); */
+
+  // reset a map of parameter names as in the model
+  adjustableParToStatus_["p"] = 1;
+  adjustableParToStatus_["omega1"] = 1;
+  adjustableParToStatus_["omega2"] = 1;
+  adjustableParToStatus_["k"] = 1;
+
+  string st;
+  for (map<string, string>::iterator it = mapParNamesFromPmodel_.begin(); it != mapParNamesFromPmodel_.end(); it++)
+  {
+    st = pmixmodel_->getParameterNameWithoutNamespace(it->first);
+    if (it->second.substr(0, 5) != "omega" && it->second.substr(0, 5) !="p")
+    {
+      addParameter_(new Parameter("RELAX." + it->second, pmixmodel_->getParameterValue(st),
+                              pmixmodel_->getParameter(st).hasConstraint() ? pmixmodel_->getParameter(st).getConstraint()->clone() : 0, true));
+    }
+  } 
+
+  /* set the below parameters that are used for parameterizing the omega parameters of the sumodels of type YN98 as autoparameters to supress exceptions when constraints of the YN98 omega parameters are exceeded
+  YN98_0.omega = (RELAX.p * RELAX.omega1) ^ RELAX.k
+  YN98_1.omega = RELAX.omega1 ^ RELAX.k  
+  YN98_2.omega = RELAX.omega2 ^ RELAX.k */
+  // reparameterization of omega0: RELAX.omega0 = RELAX.p*RELAX.omega1
+  //addParameter_(new AutoParameter("RELAX.p", 0.5, new IntervalConstraint(0.1, 1, true, true), true));
+  addParameter_(new Parameter("RELAX.p", 0.5, new IntervalConstraint(0.064, 1, true, true), true));
+  //addParameter_(new AutoParameter("RELAX.omega1", 1, new IntervalConstraint(0.64, 1, true, true), true));
+  addParameter_(new Parameter("RELAX.omega1", 1, new IntervalConstraint(0.5, 1, true, true), true));
+
+  // the upper bound of omega3 in its submodel is 999, so I must restrict upperBound(RELAX.omega2)^upperBound(RELAX.k)<=999 -> set maximal omega to 5  
+  //addParameter_(new AutoParameter("RELAX.omega2", 2, new IntervalConstraint(1, 15, true, true), true));
+  addParameter_(new Parameter("RELAX.omega2", 2, new IntervalConstraint(1, 15, true, true), true));
+
+  // add a selection intensity parameter k, which is 1 in the null case
+  //addParameter_(new AutoParameter("RELAX.k", 1, new IntervalConstraint(0, 2.5, true, true), true));
+  addParameter_(new Parameter("RELAX.k", 1, new IntervalConstraint(0, 2, true, true), true));
+
+  // look for synonymous codons
+  // assumes that the states number follow the map in the genetic code and thus:
+  // synfrom_ = index of source codon
+  // synto_ = index of destination codon
+  vector<int> supportedChars = yn98->getAlphabetStates(); // includes stop codons
+
+  for (synfrom_ = 1; synfrom_ < supportedChars.size(); ++synfrom_)
+  {
+    for (synto_ = 0; synto_ < synfrom_; ++synto_)
+    {
+      if (gc->areSynonymous(supportedChars[synfrom_], supportedChars[synto_])
+          && (pmixmodel_->getNModel(0)->Qij(synfrom_, synto_) != 0)
+          && (pmixmodel_->getNModel(1)->Qij(synfrom_, synto_) != 0))
+        break;
+    }
+    if (synto_ < synfrom_)
+      break;
+  }
+
+  if (synto_ == supportedChars.size())
+    throw Exception("Impossible to find synonymous codons");
+
+  // update the 3 rate matrices of the model (strict BG or strict FG)
+  computeFrequencies(false);
+  updateMatrices();
+}
+
+
+void RELAX::adjustBoundaries()
+{
+  // get the current values of all the parameters
+  map<string,double> parameterToValue;
+  for (map<string, bool>::iterator it = adjustableParToStatus_.begin(); it != adjustableParToStatus_.end(); it++)
+  {
+    parameterToValue[it->first] = getParameterValue(it->first);
+  }
+
+  // check which parameters have values at their boundaries, and which boundary is reached (lower or upper)
+  const IntervalConstraint* paramConstraint;
+  const IntervalConstraint* confunderConstraint;
+  IntervalConstraint* paramNewBounds; 
+  IntervalConstraint* confunderNewBounds;
+  for (map<string, bool>::iterator externalIt = adjustableParToStatus_.begin(); externalIt != adjustableParToStatus_.end(); externalIt++)
+  {
+    // get the interval constraint of the parameter
+    paramConstraint = dynamic_cast<const IntervalConstraint*>(getParameter(externalIt->first).getConstraint());
+    if (externalIt->first == "p"|| externalIt->first == "omega1") // only the lower bound can be adjusted
+    {
+      // if the conastraint is at its boundaries -> set a new inerval constraint for it and for its confunding parameters
+      if (parameterToValue[externalIt->first] == paramConstraint->getLowerBound()) // only the lower bund can be adjusted in the case of omega0 and omega1
+      {
+        // relax the lower bound of the parameter
+        paramNewBounds = new IntervalConstraint(paramConstraint->getLowerBound() - 0.01, paramConstraint->getUpperBound(), true, true);
+        getParameter_(externalIt->first).setConstraint(paramNewBounds, true);
+        adjustableParToStatus_[externalIt->first] = 0;
+        // now, adjust the bounds on the other parameters
+        for (map<string, bool>::iterator internalIt = adjustableParToStatus_.begin(); internalIt != adjustableParToStatus_.end(); internalIt++)
+          {
+          confunderConstraint = dynamic_cast<const IntervalConstraint*>(getParameter(internalIt->first).getConstraint());
+          if (internalIt->first != externalIt->first && internalIt->first != "k"  && confunderConstraint->getLowerBound() + 0.01 < confunderConstraint->getUpperBound())
+          {
+          // if the confunder's value is not at its lower bound - increase the lower bound by 0.01
+          confunderNewBounds = new IntervalConstraint(confunderConstraint->getLowerBound() + 0.01, confunderConstraint->getUpperBound(), true, true);
+          getParameter_(internalIt->first).setConstraint(confunderNewBounds, true);
+          adjustableParToStatus_[internalIt->first] = 1;
+          break;
+          }
+          else if (internalIt->first == "k" && confunderConstraint->getUpperBound() - 0.015 > confunderConstraint->getLowerBound())
+          {
+          // in the case of k, the upper bound should be adjusted - descrease its upper bound by 0.11 (k is expressed as a power and is therefore more constraining)
+          confunderNewBounds = new IntervalConstraint(confunderConstraint->getLowerBound(), confunderConstraint->getUpperBound() - 0.015, true, true);
+          getParameter_(internalIt->first).setConstraint(confunderNewBounds, true);
+          adjustableParToStatus_[internalIt->first] = 1;
+          break;
+          }
+        }
+      }
+      return;
+    }
+    else if (externalIt->first == "omega2") // only the upper bound can be adjusted
+    {
+      // if the conastraint is at its boundaries -> set a new inerval constraint for it and for its confunding parameters
+      if (parameterToValue[externalIt->first] == paramConstraint->getUpperBound()) // only the upper bound can be adjusted in the case of omega2
+      {
+        paramNewBounds = new IntervalConstraint(paramConstraint->getLowerBound(), paramConstraint->getUpperBound() + 0.01, true, true);
+        getParameter_(externalIt->first).setConstraint(paramNewBounds, true);
+        adjustableParToStatus_[externalIt->first] = 0;
+        confunderConstraint = dynamic_cast<const IntervalConstraint*>(getParameter("k").getConstraint());
+        if (confunderConstraint->getLowerBound() < confunderConstraint->getUpperBound() - 0.015)
+        {
+          confunderNewBounds = new IntervalConstraint(confunderConstraint->getLowerBound(), confunderConstraint->getUpperBound() - 0.015, true, true);
+          getParameter_("k").setConstraint(confunderNewBounds, true);
+          adjustableParToStatus_["k"] = 1;
+        }
+      }
+      return;
+    }
+    else // in the case of k - only the upper bound can be adjusted, and in addition, p/omega1 AND omega2 must be adjusted
+    {
+      // if the constraint is at its boundaries -> set a new inerval constraint for it and for its confunding parameters
+      if (parameterToValue[externalIt->first] == paramConstraint->getUpperBound()) // only the upper bound can be adjusted in the case of omega2
+      {
+        // relax the upper bound of k
+        paramNewBounds = new IntervalConstraint(paramConstraint->getLowerBound(), paramConstraint->getUpperBound() + 0.01, true, true);
+        getParameter_(externalIt->first).setConstraint(paramNewBounds, true);
+        adjustableParToStatus_[externalIt->first] = 0;
+        for (map<string, bool>::iterator internalIt = adjustableParToStatus_.begin(); internalIt != adjustableParToStatus_.end(); internalIt++)
+        {
+          confunderConstraint = dynamic_cast<const IntervalConstraint*>(getParameter(internalIt->first).getConstraint());
+          if (internalIt->first == "p" && confunderConstraint->getLowerBound() + 0.05 < confunderConstraint->getUpperBound()) // constraint the lower bound of p (to affect only omega0=p*omega1)
+          {
+            confunderNewBounds = new IntervalConstraint(confunderConstraint->getLowerBound() + 0.05, confunderConstraint->getUpperBound(), true, true);
+            getParameter_(internalIt->first).setConstraint(confunderNewBounds, true);
+            adjustableParToStatus_[internalIt->first] = 1;
+          }
+          else if (internalIt->first == "omega2" && confunderConstraint->getLowerBound() < confunderConstraint->getUpperBound() - 0.05) // constrain the upper bound of omega2 
+          {
+            confunderNewBounds = new IntervalConstraint(confunderConstraint->getLowerBound(), confunderConstraint->getUpperBound() - 0.05, true, true);
+            getParameter_(internalIt->first).setConstraint(confunderNewBounds, true);
+            adjustableParToStatus_[internalIt->first] = 1;
+          }
+        }
+      }
+      return;
+    }
+  }
+}
+
+
+void RELAX::updateMatrices()
+{
+  // update the values of the sub-model parameters, that are used in the 3 rate matrices
+  for (unsigned int i = 0; i < lParPmodel_.size(); i++)
+  {
+    // first update the values of the non omega patrameters
+    const string& np = lParPmodel_[i].getName();
+    if (mapParNamesFromPmodel_.find(np) != mapParNamesFromPmodel_.end())
+    { 
+      if (np.size()>19 && np[18] == 'V')
+      {
+        double k = getParameterValue("k"); // get the value of k
+        int ind = -1 ;
+        if (np.size()>19) {
+          ind = TextTools::to<int>(np.substr(19)) - 1; // ind corresponds to the index of the omega that belongs to submodel ind+1
+        }
+        // change ind not to be -1 to allow names omega1, omega2, omega3
+        if (ind > 0) {                    // raise each omega to the power of k
+          double omega =  getParameterValue("omega" + TextTools::toString(ind));
+          omega = pow (omega, k);
+          lParPmodel_[i].setValue(omega);
+        } else if (ind == 0) {                          // handle omega0 differently due to reparameterization via RELAX.p
+          double omega1 = getParameterValue("omega1");
+          double p = getParameterValue("p");
+          double omega0 = p * omega1;
+          omega0 = pow(omega0,k);
+          lParPmodel_[i].setValue(omega0); // update the omega of the first sub-model according to the transformation: omega0^k = (p*omega1)^k
+        }
+      }
+      else
+      {
+        lParPmodel_[i].setValue(getParameter(getParameterNameWithoutNamespace(mapParNamesFromPmodel_[np])).getValue());
+      }
+    }
+  }
+
+  pmixmodel_->matchParametersValues(lParPmodel_);
+
+  // normalize the synonymous substitution rate in all the Q matrices of the 3 submodels to be the same
+  Vdouble vd;
+
+  for (unsigned int i = 0; i < pmixmodel_->getNumberOfModels(); i++)
+  {
+    vd.push_back(1 / pmixmodel_->getNModel(i)->Qij(synfrom_, synto_));
+  }
+
+  pmixmodel_->setVRates(vd);
+
+  //adjust the boundaries of the parameters so that they would not have values at their boundaries 
+  //adjustBoundaries();
+}
---
+++ src/Bpp/Phyl/Model/Codon/RELAX.h
@@ -0,0 +1,112 @@
+//
+// File: RELAX.h
+// Created by: Keren Halabi
+// Created on: May 2018
+//
+
+/*
+  Copyright or © or Copr. CNRS, (November 16, 2004)
+
+  This software is a computer program whose purpose is to provide classes
+  for phylogenetic data analysis.
+
+  This software is governed by the CeCILL  license under French law and
+  abiding by the rules of distribution of free software.  You can  use, 
+  modify and/ or redistribute the software under the terms of the CeCILL
+  license as circulated by CEA, CNRS and INRIA at the following URL
+  "http://www.cecill.info". 
+
+  As a counterpart to the access to the source code and  rights to copy,
+  modify and redistribute granted by the license, users are provided only
+  with a limited warranty  and the software's author,  the holder of the
+  economic rights,  and the successive licensors  have only  limited
+  liability. 
+
+  In this respect, the user's attention is drawn to the risks associated
+  with loading,  using,  modifying and/or developing or reproducing the
+  software by the user in light of its specific status of free software,
+  that may mean  that it is complicated to manipulate,  and  that  also
+  therefore means  that it is reserved for developers  and  experienced
+  professionals having in-depth computer knowledge. Users are therefore
+  encouraged to load and test the software's suitability as regards their
+  requirements in conditions enabling the security of their systems and/or 
+  data to be ensured and,  more generally, to use and operate it in the 
+  same conditions as regards security. 
+
+  The fact that you are presently reading this means that you have had
+  knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#ifndef _RELAX_H_
+#define _RELAX_H_
+
+
+#include <Bpp/Phyl/Model/Codon/YNGP_M.h>
+#include <Bpp/Seq/GeneticCode/GeneticCode.h>
+
+namespace bpp
+{
+
+/**
+ * @brief The RELAX (2014) branch-site model for codons
+ * The model detects changes in selective pressure based on a prior partition of the tree branches provided by the user
+ * @author Keren Halabi
+ *
+ * This model cocnsists of a mixture of models, each defined as described in YN98 class (will use kappa instead of 5 GTR parameters),
+ * The mixture is defined in two levels: the site level and the branch level
+ * The model consists of two site models - each one for a different branches group: brackground and foreground
+ * The site model of the background group is a mixture of 3 MG94 models 
+ * The YN98 (i.e, kappa) parameters, except for the omega value, are shared between the 3 sub-models
+ * Each site can be assigned to one of 3 omega classes that correspond to the 3 selective regimes:
+ * \omega_0 = omega_1 * p < 1 @f$ (with probability @f$p_0 @f$)
+ * \omega_1 <= 1 @f$ (with probability @f$p_1 @f$)
+ * \omega_2 > 1 @f$ (with probability 1-@f$p_1-@f$p_0 @f$)
+ * The omegas of the foreground group are obtained by raising the omegas of the background group to the power of a selection intensity parameter k
+ * Overall, the model consists of 9 parameters, base frequencies parameters excluded:
+ * 5 parameters for the GTR model that is nested in the MG94 model (Currently, the model is implemented with a single kappa parameter rather than 5 GTR parameters)
+ * 3 omega values parameters + 2 omega frequencies parameters
+ * 1 selection intensity parameter k
+ * 
+ * References:
+ *
+ * Joel O., et al. "RELAX: detecting relaxed selection in a phylogenetic framework." 
+ * Molecular biology and evolution 32.3 (2014): 820-832.‏
+ */
+  class RELAX:
+    public YNGP_M
+  {
+  public:
+    // gc is a pointer to a constant GeneticCode instance
+    // unlike GeneticCode const* gc - gc is a constant pointer to a GeneticCode instance
+    // see: https://stackoverflow.com/questions/3984989/what-is-the-differnce-between-const-x-a-and-x-const-a-if-x-is-the-class?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+    RELAX(const GeneticCode* gc, FrequenciesSet* codonFreqs);
+
+    RELAX* clone() const { return new RELAX(*this); }
+
+    RELAX(const RELAX& mod2) :
+      YNGP_M(mod2),
+      adjustableParToStatus_()
+    {
+    }
+
+    RELAX& operator=(const RELAX& mod2)
+    {
+      YNGP_M::operator=(mod2);
+      return *this;
+    }
+
+  protected:
+    std::map<std::string,bool> adjustableParToStatus_; // status can be 0 == constaint relaxed (requires union during aliasing), or 1 == contraint remains the same or intensified (requires intersection during aliasing)
+    bool getAdjustedParamStatus(std::string parName) { return adjustableParToStatus_[parName]; }
+    void updateMatrices();
+    void adjustBoundaries(); // adjusts boundaries n the fly during optimization: changes the boundaries of parameters whose lower or upper bounds are set as their current value on the expense of other confunding parameters in the model
+
+  public:
+    std::string getName() const { return "RELAX"; }
+
+  };
+
+} //end of namespace bpp.
+
+#endif	// _RELAX_H
+
