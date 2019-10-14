11978afaa876cef9df8961bcd548e359ec7e478b
Laurent Gueguen <laurent.gueguen@univ-lyon1.fr>
2019-10-14 23:25:18 +0200

    fix weights error in DecompositionSubstitutionCount

--- src/Bpp/Phyl/Mapping/DecompositionSubstitutionCount.cpp
+++ src/Bpp/Phyl/Mapping/DecompositionSubstitutionCount.cpp
@@ -104,12 +104,11 @@ void DecompositionSubstitutionCount::computeCounts_(double length) const
     for (size_t j = 0; j < nbStates_; j++) {
       for (size_t k = 0; k < nbStates_; k++) {
         counts_[i](j, k) /= P(j, k);
-        if (std::isinf(counts_[i](j, k)) || std::isnan(counts_[i](j, k)) || counts_[i](j, k) < 0.) {
+        if (std::isinf(counts_[i](j, k)) || std::isnan(counts_[i](j, k)) || counts_[i](j, k) < 0.)
           counts_[i](j, k) = 0.;
-          //Weights:
-          if (weights_)
-            counts_[i](j, k) *= weights_->getIndex(supportedStates[j], supportedStates[k]);
-        }
+        //Weights:
+        if (weights_)
+          counts_[i](j, k) *= weights_->getIndex(supportedStates[j], supportedStates[k]);
       }
     }
   }
