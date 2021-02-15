f29cee2090cd074e6af0c37c2786be4c724f0309
anatshafir1 <anatshafir1@mail.tau.ac.il>
2021-02-15 12:03:15 +0200

    Adding weighted root frequencies feature

--- src/Bpp/Phyl/NewLikelihood/DataFlow/LikelihoodCalculationSingleProcess.cpp
+++ src/Bpp/Phyl/NewLikelihood/DataFlow/LikelihoodCalculationSingleProcess.cpp
@@ -449,12 +449,12 @@ void LikelihoodCalculationSingleProcess::makeRootFreqs_()
 // Set root frequencies 
   size_t nbState = getStateMap().getNumberOfModelStates();
   if (weightedRootFrequencies_){
-      std::vector<double> weightedRootFreqs = findWeightedRootFrequencies();
-      setWeightedRootFrequencies(weightedRootFreqs);
-      //auto sumOfWeights = CWiseAdd<double, Eigen::RowVectorXd>::create(getContext_(), {vRateCatTrees_[0].flt->getForwardLikelihoodArrayAtRoot()}, Dimension<double>());
-      //auto inverseSumOfWeights = CWiseInverse<double>::create(getContext_(), {sumOfWeights}, Dimension<double>());
-      //rFreqs_ = CWiseDiv<Eigen::RowVectorXd, std::tuple<Eigen::RowVectorXd, double>>::create(getContext_(),{vRateCatTrees_[0].flt->getForwardLikelihoodArrayAtRoot(), sumOfWeights}, RowVectorDimension(Eigen::Index(nbState)));
-      //rFreqs_ = CWiseMul<Eigen::RowVectorXd, std::tuple<double, Eigen::RowVectorXd>>::create(getContext_(),{inverseSumOfWeights, vRateCatTrees_[0].flt->getForwardLikelihoodArrayAtRoot()}, RowVectorDimension (Eigen::Index (nbState)));
+      auto sumOfWeights = CWiseAdd<double, Eigen::RowVectorXd>::create(getContext_(), {vRateCatTrees_[0].flt->getForwardLikelihoodArrayAtRoot()}, Dimension<double>());
+      // warning: this is correct only for one site!!!!
+      size_t nbSite = vRateCatTrees_[0].flt->getForwardLikelihoodArrayAtRoot()->getTargetValue().cols();
+      auto converted = Convert<Eigen::MatrixXd, Transposed<Eigen::MatrixXd>>::create(getContext_(), {vRateCatTrees_[0].flt->getForwardLikelihoodArrayAtRoot()}, MatrixDimension (nbSite, nbState));
+      auto rootCondLik = CWiseAdd<Eigen::RowVectorXd, Eigen::MatrixXd>::create(getContext_(), {converted}, RowVectorDimension(Eigen::Index(nbState)));
+      rFreqs_ = CWiseDiv<Eigen::RowVectorXd, std::tuple<Eigen::RowVectorXd, double>>::create(getContext_(),{rootCondLik, sumOfWeights}, RowVectorDimension(Eigen::Index(nbState)));
   }else{
     
     rFreqs_ = processNodes_.rootFreqsNode_?ConfiguredParametrizable::createRowVector<ConfiguredFrequencySet, FrequenciesFromFrequencySet> (
