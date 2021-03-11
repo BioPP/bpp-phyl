8302ef83cda954cc6c1f5e84fd2f7a65ce96770e
anatshafir1 <anatshafir1@mail.tau.ac.il>
2021-03-11 19:22:55 +0200

    cleaning the test file from ExtendedFloat tests

--- test/test_chr_model.cpp
+++ test/test_chr_model.cpp
@@ -11,6 +11,7 @@
 #include <Bpp/Phyl/Model/ChromosomeSubstitutionModel.h>
 #include <Bpp/Seq/Container/VectorSequenceContainer.h>
 #include <Bpp/Seq/Container/VectorSiteContainer.h>
+#include <Bpp/Phyl/NewLikelihood/DataFlow/ExtendedFloatTools.h>
 #include <string>
 #include <vector>
 #include <iostream>
@@ -54,7 +55,7 @@ int main(){
 
     const ChromosomeAlphabet* alphabet = new ChromosomeAlphabet(1,5);
     ChromosomeSubstitutionModel* model1 = new ChromosomeSubstitutionModel(alphabet, 2, 1, 3, 1.3, 0.0123, 0.35, IgnoreParam, IgnoreParam, IgnoreParam, 4, ChromosomeSubstitutionModel::rootFreqType::ROOT_LL, ChromosomeSubstitutionModel::LINEAR);
-    ChromosomeSubstitutionModel* model2 = new ChromosomeSubstitutionModel(alphabet, 2, 1, 3, 1.3, 0.0123, 0.35, IgnoreParam, IgnoreParam, IgnoreParam, 4, ChromosomeSubstitutionModel::rootFreqType::ROOT_LL, ChromosomeSubstitutionModel::LINEAR, true);
+    //ChromosomeSubstitutionModel* model2 = new ChromosomeSubstitutionModel(alphabet, 2, 1, 3, 1.3, 0.0123, 0.35, IgnoreParam, IgnoreParam, IgnoreParam, 4, ChromosomeSubstitutionModel::rootFreqType::ROOT_LL, ChromosomeSubstitutionModel::LINEAR, true);
     RowMatrix<double> Qij;
     MatrixTools::copy(model1->getGenerator(), Qij);
     MatrixTools::print(Qij);
@@ -69,29 +70,29 @@ int main(){
     RowMatrix <double> pijdt;
     RowMatrix <double> pijtdt2;
 
-    MatrixXef pijt_ef;
-    MatrixXef pijdt_ef;
-    MatrixXef pijtdt2_ef;
+    //MatrixXef pijt_ef;
+    //MatrixXef pijdt_ef;
+    //MatrixXef pijtdt2_ef;
 
     for (size_t i = 0; i < t.size(); i++){
         std::cout << "********************************" << endl;
         std::cout << "********************************" << endl;
         std::cout << "Starting with t = " << t[i] << endl;
         pijt = model1->getPijt_test(t[i]);
-        pijt_ef = model2->getPijtEf_test(t[i]);
+        //pijt_ef = model2->getPijtEf_test(t[i]);
         pijdt = model1->getdPij_dt(t[i]);
-        pijdt_ef = model2->getdPijEf_dt(t[i]);
+        //pijdt_ef = model2->getdPijEf_dt(t[i]);
         pijtdt2 = model1->getd2Pij_dt2(t[i]);
-        pijtdt2_ef = model2->getd2PijEf_dt2(t[i]);
+        //pijtdt2_ef = model2->getd2PijEf_dt2(t[i]);
 
         Eigen::MatrixXd pijt_db = ExtendedFloatTools::convertBppMatrixToEigenDB(pijt);
-        Eigen::MatrixXd pijt_ef_db = ExtendedFloatTools::convertMatToDouble(pijt_ef);
+        //Eigen::MatrixXd pijt_ef_db = ExtendedFloatTools::convertMatToDouble(pijt_ef);
 
         Eigen::MatrixXd pijdt_db = ExtendedFloatTools::convertBppMatrixToEigenDB(pijdt);
-        Eigen::MatrixXd pijdt_ef_db = ExtendedFloatTools::convertMatToDouble(pijdt_ef);
+        //Eigen::MatrixXd pijdt_ef_db = ExtendedFloatTools::convertMatToDouble(pijdt_ef);
 
         Eigen::MatrixXd pijtdt2_db = ExtendedFloatTools::convertBppMatrixToEigenDB(pijtdt2);
-        Eigen::MatrixXd pijtdt2_ef_db = ExtendedFloatTools::convertMatToDouble(pijtdt2_ef);
+        //Eigen::MatrixXd pijtdt2_ef_db = ExtendedFloatTools::convertMatToDouble(pijtdt2_ef);
 
 
         std::cout << "***" << endl;
@@ -99,25 +100,25 @@ int main(){
         std::cout << pijt_db << endl;
         std::cout << "***" << endl;
         std::cout << "Pijt Extended float:" << endl;
-        std::cout << pijt_ef_db << endl;
+        //std::cout << pijt_ef_db << endl;
         std::cout << "******************************" << endl;
         std::cout << "Pijdt double:" << endl;
         std::cout << pijdt_db << endl;
         std::cout << "***" << endl;
         std::cout << "Pijdt Extended float:" << endl;
-        std::cout << pijdt_ef_db << endl;
+        //std::cout << pijdt_ef_db << endl;
         std::cout << "******************************" << endl;
         std::cout << "pijtd2t2 double:" << endl;
         std::cout << pijtdt2_db << endl;
         std::cout << "***" << endl;
         std::cout << "Pijd2t2 Extended float:" << endl;
-        std::cout << pijtdt2_ef_db << endl;
+        //std::cout << pijtdt2_ef_db << endl;
         std::cout << "******************************" << endl;
 
     }
  
     delete model1;
-    delete model2;
+    //delete model2;
     delete alphabet;
 
 
